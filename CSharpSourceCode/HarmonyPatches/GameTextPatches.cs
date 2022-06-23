d238f27faf6fca85c1ab80d69770f889cec05db2
Péter Haraszti <hunharibo@gmail.com>
2022-06-23 23:40:28 +0200

    added strings

---
+++ CSharpSourceCode/HarmonyPatches/GameTextPatches.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+
+namespace TOR_Core.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class GameTextPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GameText), "AddVariationWithId")]
+        public static void LoadXmlWithOverride(List<GameText.GameTextVariation> ____variationList, string variationId, TextObject text, List<GameTextManager.ChoiceTag> choiceTags)
+        {
+            var list = ____variationList.Where(x => x.Id == variationId).ToList();
+            if(list.Count() > 0)
+            {
+                foreach(var item in list)
+                {
+                    ____variationList.Remove(item);
+                }
+            }
+		}
+    }
+}
