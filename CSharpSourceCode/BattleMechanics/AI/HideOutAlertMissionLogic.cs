082796992ba8540418a7d88d24ee82353bcb6302
Linus Tiemann <linus-tiemann@live.de>
2022-09-04 11:24:27 +0200

    fix intendentation and removed base method

--- CSharpSourceCode/BattleMechanics/AI/HideOutAlertMissionLogic.cs
+++ CSharpSourceCode/BattleMechanics/AI/HideOutAlertMissionLogic.cs
@@ -4,23 +4,25 @@ using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 
 
-    public class HideoutAlertMissionLogic : MissionLogic
+public class HideoutAlertMissionLogic : MissionLogic
+{
+    private bool _enemiesAreAlarmed;
+
+    public override void OnAgentShootMissile(Agent shooterAgent, EquipmentIndex weaponIndex, Vec3 position,
+        Vec3 velocity, Mat3 orientation,
+        bool hasRigidBody, int forcedMissileIndex)
     {
-        private bool _enemiesAreAlarmed;
-        public override void OnAgentShootMissile(Agent shooterAgent, EquipmentIndex weaponIndex, Vec3 position, Vec3 velocity, Mat3 orientation,
-                bool hasRigidBody, int forcedMissileIndex) 
+        if (_enemiesAreAlarmed) return;
+        var itemUsage = shooterAgent.WieldedWeapon.CurrentUsageItem.ItemUsage;
+        if (!itemUsage.Contains("handgun") && !itemUsage.Contains("pistol")) return;
+        var hideoutMissionController = Mission.Current.GetMissionBehavior<HideoutMissionController>();
+        if (hideoutMissionController == null) return;
+        foreach (var agent in base.Mission.PlayerEnemyTeam.TeamAgents)
         {
-                base.OnAgentShootMissile(shooterAgent, weaponIndex, position, velocity, orientation, hasRigidBody, forcedMissileIndex);
-                if (_enemiesAreAlarmed) return;
-                var itemUsage = shooterAgent.WieldedWeapon.CurrentUsageItem.ItemUsage;
-                if (!itemUsage.Contains("handgun") && !itemUsage.Contains("pistol")) return;
-                var hideoutMissionController = Mission.Current.GetMissionBehavior<HideoutMissionController>();
-                if (hideoutMissionController == null) return;
-                foreach (var agent in base.Mission.PlayerEnemyTeam.TeamAgents)
-                {
-                    hideoutMissionController.OnAgentAlarmedStateChanged(agent, Agent.AIStateFlag.Alarmed);
-                    agent.SetWatchState(Agent.WatchState.Alarmed);
-                }
-                _enemiesAreAlarmed=true; 
+            hideoutMissionController.OnAgentAlarmedStateChanged(agent, Agent.AIStateFlag.Alarmed);
+            agent.SetWatchState(Agent.WatchState.Alarmed);
         }
+
+        _enemiesAreAlarmed = true;
     }
+}
