05c79c11aaa43ed1f80ae197a04ec170d1493919
Mart Kartašev <mart.kartasev@gmail.com>
2022-11-05 15:55:10 +0100

    Debug

--- CSharpSourceCode/Extensions/DebugMethods.cs
+++ CSharpSourceCode/Extensions/DebugMethods.cs
@@ -5,12 +5,12 @@ namespace TOR_Core.Extensions
 {
     public static class DebugMethods
     {
-        public static void DebugRender(this Target target)
+        public static void DebugRender(this Target target, float time = 30, float size = 2)
         {
             if (target.TacticalPosition != null)
             {
-                MBDebug.RenderDebugText3D(target.TacticalPosition.Position.GetGroundVec3(), target.UtilityValue.ToString(), 4294967295U, 0, 0, 30);
-                MBDebug.RenderDebugSphere(target.TacticalPosition.Position.GetGroundVec3(), 2, 4294967295, false, 30);
+                MBDebug.RenderDebugText3D(target.TacticalPosition.Position.GetGroundVec3(), target.UtilityValue.ToString(), 4294967295U, 0, 0, time);
+                MBDebug.RenderDebugSphere(target.TacticalPosition.Position.GetGroundVec3(), size, 4294967295, false, time);
                 MBDebug.RenderDebugDirectionArrow(target.TacticalPosition.Position.GetGroundVec3(), target.TacticalPosition.Direction.ToVec3());
             }
         }
