3a8a4b355deac896bb6fc5e178a856185877ffdc
Linus Tiemann <linus-tiemann@live.de>
2023-02-13 22:06:19 +0100

    cleanup 3

--- CSharpSourceCode/Extensions/AgentDrivenPropertiesExtensions.cs
+++ CSharpSourceCode/Extensions/AgentDrivenPropertiesExtensions.cs
@@ -6,26 +6,21 @@ namespace TOR_Core.Extensions
 {
     public static class AgentDrivenPropertiesExtensions
     {
-
         public static void SetDynamicCombatProperties(this AgentDrivenProperties agentDrivenProperties, StatusEffectComponent statusEffectComponent, float multiplier)
         {
-            agentDrivenProperties.SwingSpeedMultiplier = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.SwingSpeedMultiplier)*multiplier;
+            agentDrivenProperties.SwingSpeedMultiplier = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.SwingSpeedMultiplier) * multiplier;
         }
 
         public static void SetDynamicHumanoidMovementProperties(this AgentDrivenProperties agentDrivenProperties, StatusEffectComponent statusEffectComponent, float multiplier)
         {
-            agentDrivenProperties.MaxSpeedMultiplier =statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MaxSpeedMultiplier)*multiplier;
+            agentDrivenProperties.MaxSpeedMultiplier = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MaxSpeedMultiplier) * multiplier;
         }
-        
+
         public static void SetDynamicMountMovementProperties(this AgentDrivenProperties agentDrivenProperties, StatusEffectComponent statusEffectComponent, float multiplier)
         {
-            agentDrivenProperties.MountSpeed = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MountSpeed)*multiplier;
-            agentDrivenProperties.MountDashAccelerationMultiplier=statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MountDashAccelerationMultiplier)*multiplier;
-            agentDrivenProperties.MountManeuver = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MountManeuver)*multiplier;
+            agentDrivenProperties.MountSpeed = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MountSpeed) * multiplier;
+            agentDrivenProperties.MountDashAccelerationMultiplier = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MountDashAccelerationMultiplier) * multiplier;
+            agentDrivenProperties.MountManeuver = statusEffectComponent.GetBaseValueForDrivenProperty(DrivenProperty.MountManeuver) * multiplier;
         }
-        
     }
-    
-    
-    
 }
