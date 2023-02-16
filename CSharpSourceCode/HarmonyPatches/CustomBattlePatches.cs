8085608578af3d301c5c339741210e7e4ba1b7b9
Linus Tiemann <linus-tiemann@live.de>
2023-02-16 23:15:27 +0100

    beast magic WIP

--- CSharpSourceCode/HarmonyPatches/CustomBattlePatches.cs
+++ CSharpSourceCode/HarmonyPatches/CustomBattlePatches.cs
@@ -30,6 +30,7 @@ namespace TOR_Core.HarmonyPatches
                 list.Add(Game.Current.ObjectManager.GetObject<BasicCharacterObject>("tor_cw_lord"));
                 list.Add(Game.Current.ObjectManager.GetObject<BasicCharacterObject>("tor_necromancer_lord"));
                 list.Add(Game.Current.ObjectManager.GetObject<BasicCharacterObject>("tor_prophetess_lw"));
+                list.Add(Game.Current.ObjectManager.GetObject<BasicCharacterObject>("tor_prophetess_bw"));
             }
             catch (Exception e)
             {
