b9d2d8e32efa032aec0033ed7be4e05b4f2dc91b
Linus Tiemann <linus-tiemann@live.de>
2023-03-05 13:08:04 +0100

    removed unrelated classes

--- CSharpSourceCode/HarmonyPatches/YearPatch.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TOR_Core.Models.CustomBattleModels
-{
-    public class YearPatch
-    {
-        static AccessTools.FieldRef<MapEventParty, FlattenedTroopRoster> currentRoster =
-            AccessTools.FieldRefAccess<MapEventParty, FlattenedTroopRoster>("_roster");
-    }
-}
