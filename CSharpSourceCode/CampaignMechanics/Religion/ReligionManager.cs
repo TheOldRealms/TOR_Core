5ad7e3d6db35f3d71b1d4b878cc99517078afd9a
Péter Haraszti <hunharibo@gmail.com>
2023-03-16 11:22:59 +0100

    religion objects

---
+++ CSharpSourceCode/CampaignMechanics/Religion/ReligionManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOR_Core.CampaignMechanics.Religion
+{
+    public class ReligionManager
+    {
+        private static ReligionManager instance;
+
+        private ReligionManager() { }
+
+        public static void Initialize()
+        {
+            instance = new ReligionManager();
+        }
+    }
+}
---
+++ CSharpSourceCode/CampaignMechanics/Religion/ReligionObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TaleWorlds.Core;
+
+namespace TOR_Core.CampaignMechanics.Religion
+{
+    [Serializable]
+    public class ReligionObject : IDeserializationCallback
+    {
+        [XmlAttribute]
+        public string StringId { get; set; }
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public string CultureId { get; set; }
+        [XmlIgnore]
+        public string LoreText { get; set; }
+
+        public void OnDeserialization(object sender)
+        {
+            LoreText = GameTexts.FindText("religion_lore_text", StringId).ToString();
+        }
+    }
+}
