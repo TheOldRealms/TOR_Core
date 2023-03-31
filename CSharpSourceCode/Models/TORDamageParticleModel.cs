8feaf6a93258d3a9df75cdb39ba2859518d8c7e8
Linus Tiemann <linus-tiemann@live.de>
2023-04-01 01:50:02 +0200

    test for non sticking arrows (working prototype)

--- CSharpSourceCode/Models/TORDamageParticleModel.cs
+++ CSharpSourceCode/Models/TORDamageParticleModel.cs
@@ -1,4 +1,6 @@
-﻿using TaleWorlds.MountAndBlade;
+﻿using System.Linq;
+using TaleWorlds.MountAndBlade;
+using TOR_Core.BattleMechanics.StatusEffect;
 using TOR_Core.Extensions;
 
 namespace TOR_Core.Models
@@ -33,11 +35,31 @@ namespace TOR_Core.Models
         }
         public override int GetMissileAttackParticle(Agent attacker, Agent victim, in Blow blow, in AttackCollisionData collisionData)
         {
+            if (victim.GetComponent<StatusEffectComponent>()!=null)
+            {
+                if (victim.GetComponent<StatusEffectComponent>().HasTemporaryAttribute("ClearBloodBurst"))
+                {
+                    HideMissleIfExists(blow.WeaponRecord.AffectorWeaponSlotOrMissileIndex);
+                    return -1;
+                }
+                   
+            }
+            
             if (victim.IsUndead())
             {
                 return -1;
             }
             return base.GetMissileAttackParticle(attacker, victim, blow, collisionData);
+        }
+
+
+
+
+        private void HideMissleIfExists(int index)
+        {
+            var missle = Mission.Current.Missiles.FirstOrDefault(x=> x.Index==index);
+            if(missle!=null)
+                missle.Entity.SetAlpha(0);
         }
     }
 }
