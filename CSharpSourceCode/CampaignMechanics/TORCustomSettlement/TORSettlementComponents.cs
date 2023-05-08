a56e0b8daf9454cb76c53cdb3bc9b81f6a6c0a6f
Péter Haraszti <hunharibo@gmail.com>
2023-05-08 23:00:40 +0200

    add back areamarker

--- CSharpSourceCode/CampaignMechanics/TORCustomSettlement/TORSettlementComponents.cs
+++ CSharpSourceCode/CampaignMechanics/TORCustomSettlement/TORSettlementComponents.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using SandBox;
+using SandBox.View.Map;
+using System;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
 using TaleWorlds.ObjectSystem;
+using TaleWorlds.ScreenSystem;
 using TOR_Core.CampaignMechanics.RaidingParties;
 using TOR_Core.Utilities;
 
 namespace TOR_Core.CampaignMechanics.TORCustomSettlement
 {
-    public class CursedSiteComponent : TORBaseSettlementComponent
+    public class CursedSiteComponent : TORBaseSettlementComponent, IDisposable
     {
         private int _wardHours = 0;
+        private bool _isMarkerShown = false;
+        private GameEntity _markerEntity;
+        private Decal _markerDecal;
         public int WardHours
         {
             get { return _wardHours; }
@@ -22,6 +31,67 @@ namespace TOR_Core.CampaignMechanics.TORCustomSettlement
             }
         }
         public void HourlyTick() => WardHours = Math.Max(0, WardHours - 1);
+
+        public override void OnInit()
+        {
+            base.OnInit();
+            InformationManager.OnShowTooltip += OnShowTooltip;
+            InformationManager.OnHideTooltip += OnHideTooltip;
+        }
+
+        private void OnShowTooltip(Type type, object[] args)
+        {
+            if (type == typeof(Settlement) && ScreenManager.TopScreen is MapScreen)
+            {
+                var settlement = args[0] as Settlement;
+                if (settlement == Settlement)
+                {
+                    ShowAreaMarker(true);
+                }
+            }
+        }
+
+        private void OnHideTooltip()
+        {
+            if (_isMarkerShown) ShowAreaMarker(false);
+        }
+
+        private void ShowAreaMarker(bool shouldShow)
+        {
+            if (_markerEntity == null) CreateVisuals();
+            if (_markerEntity != null)
+            {
+                MatrixFrame frame = Settlement.Party.Visuals.GetGlobalFrame();
+                frame.Scale(new Vec3(32, 32, 1));
+                _markerEntity.SetGlobalFrame(frame);
+                _markerDecal.SetFactor1Linear(4281663744U);
+                _markerEntity.SetVisibilityExcludeParents(shouldShow);
+            }
+            _isMarkerShown = shouldShow;
+        }
+
+        private void CreateVisuals()
+        {
+            MapScene mapScene = Campaign.Current.MapSceneWrapper as MapScene;
+            _markerEntity = GameEntity.CreateEmpty(mapScene.Scene, true);
+            _markerEntity.Name = "CursedSiteMarker";
+            _markerDecal = Decal.CreateDecal();
+            if (_markerDecal != null && _markerEntity != null)
+            {
+                Material resource = Material.GetFromResource("decal_city_circle_a");
+                _markerDecal.SetMaterial(resource);
+                mapScene.Scene.AddDecalInstance(_markerDecal, "editor_set", false);
+                _markerEntity.AddComponent(_markerDecal);
+            }
+        }
+
+        public void Dispose()
+        {
+            InformationManager.OnShowTooltip -= OnShowTooltip;
+            InformationManager.OnHideTooltip -= OnHideTooltip;
+            _markerDecal = null;
+            _markerEntity = null;
+        }
     }
 
     public class ShrineComponent : TORBaseSettlementComponent { }
