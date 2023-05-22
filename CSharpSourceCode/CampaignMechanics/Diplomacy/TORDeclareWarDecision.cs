41c7fedba5b5c3e4984f1e9d3ad1d101e0d7f487
Mart Kartašev <mart.kartasev@gmail.com>
2023-05-22 10:25:49 +0200

    Should all of this go in declare war decision?

--- CSharpSourceCode/CampaignMechanics/Diplomacy/TORDeclareWarDecision.cs
+++ CSharpSourceCode/CampaignMechanics/Diplomacy/TORDeclareWarDecision.cs
@@ -12,16 +12,211 @@ namespace TOR_Core.CampaignMechanics.Diplomacy
 
         public override float DetermineSupport(Clan clan, DecisionOutcome possibleOutcome)
         {
-            Hero leader = clan.Leader;
-            DeclareWarDecisionOutcome warDecisionOutcome = (DeclareWarDecisionOutcome)possibleOutcome;
-            float denarsToInfluence = Campaign.Current.Models.DiplomacyModel.DenarsToInfluence();
-            float num = new DeclareWarBarterable(Kingdom, FactionToDeclareWarOn).GetValueForFaction(clan) * denarsToInfluence;
-            return warDecisionOutcome.ShouldWarBeDeclared ? num + (leader.GetTraitLevel(DefaultTraits.Valor) * 20 - leader.GetTraitLevel(DefaultTraits.Mercy) * 10) : -num + (-leader.GetTraitLevel(DefaultTraits.Valor) * 20 + leader.GetTraitLevel(DefaultTraits.Mercy) * 10);
+            var baseValue =  base.DetermineSupport(clan, possibleOutcome);
+            var ownClanHeroes = clan.Heroes;
+
+            float religionValue= 0f;
+            
+            foreach (var hero in ownClanHeroes)
+            {
+                var otherSideHeroes = FactionToDeclareWarOn.Heroes;
+                foreach (var enemy in otherSideHeroes)
+                {
+                    foreach (var religion in ReligionObject.All)
+                    {
+                        if(hero.GetDevotionLevelForReligion(religion) == DevotionLevel.None)
+                            continue;
+                        foreach (var comparedToReligion in ReligionObject.All)
+                        {
+                            religionValue += DeterminePositiveEffect(hero, religion, enemy, comparedToReligion);
+                            religionValue += DetermineNegativeEffect(hero, religion, enemy, comparedToReligion);
+                        }
+                    }
+                }
+            }
+
+            religionValue = religionValue / FactionToDeclareWarOn.Heroes.Count;
+            
+
+            return baseValue;
         }
 
         public new float CalculateSupport(Clan clan)
         {
             return DetermineSupport(clan, new DeclareWarDecisionOutcome(true, Kingdom, FactionToDeclareWarOn));
+        }
+        
+        
+        private float DeterminePositiveEffect(Hero hero, ReligionObject religion, Hero enemy, ReligionObject comparedToReligion)
+        {
+            if (religion.HostileReligions.Contains(comparedToReligion))
+                return 0;
+
+            var value = 0;
+            var heroDevotion = hero.GetDevotionLevelForReligion(religion);
+            var enemyDevotion = enemy.GetDevotionLevelForReligion(comparedToReligion);
+
+            var shareAffinity = religion.Affinity == comparedToReligion.Affinity;
+            var isSame = religion.Name == comparedToReligion.Name;
+
+            if (isSame)
+            {
+                switch (heroDevotion)
+                {
+                    case DevotionLevel.Fanatic:
+                        if (enemyDevotion==DevotionLevel.Fanatic)
+                        {
+                            value += 100;
+                        }
+                        break;
+                    case DevotionLevel.Devoted:
+                        if (enemyDevotion== DevotionLevel.Fanatic)
+                            value += 25;
+                        else
+                            value += 50;
+                        break;
+                    case DevotionLevel.Follower:
+                        if (enemyDevotion!= DevotionLevel.Fanatic)
+                            value += 10;
+                        break;
+                    case DevotionLevel.None:
+                        break;
+                }
+                
+            }
+            else if(shareAffinity)
+            {
+                switch (heroDevotion)
+                {
+                    case DevotionLevel.Fanatic:
+                        if (enemyDevotion==DevotionLevel.Fanatic)
+                            value += 10;
+                        break;
+                    case DevotionLevel.Devoted:
+                        if (enemyDevotion== DevotionLevel.Fanatic)
+                            value += 25;
+                        else
+                            value += 50;
+                        break;
+                    case DevotionLevel.Follower:
+                        if (enemyDevotion== DevotionLevel.Fanatic)
+                            value += 0;
+                        else
+                            value += 25;
+                        break;
+                    case DevotionLevel.None:
+                        break;
+                }
+            }
+            return value;
+        }
+        
+        private float DetermineNegativeEffect(Hero hero, ReligionObject religion, Hero enemy, ReligionObject comparedToReligion)
+        {
+            var value = 0;
+            
+            var heroDevotion = hero.GetDevotionLevelForReligion(religion);
+            var enemyDevotion = enemy.GetDevotionLevelForReligion(comparedToReligion);
+
+            if (heroDevotion == DevotionLevel.None) //If a hero is not devoted at all, he should also not care about any hostility.
+                return value;
+            
+            var shareAffinity = religion.Affinity == comparedToReligion.Affinity;
+            var isSame = religion.Name == comparedToReligion.Name;
+
+            if (isSame)     //The Person is only hostile if he is fanatic about his religion, and thinks other don't follow enough
+            {
+                switch (heroDevotion)
+                {
+                    case DevotionLevel.Fanatic:
+                        if (enemyDevotion == DevotionLevel.None)
+                        {
+                            value += -20;
+                        }
+                        break;
+                }
+            }
+            else if (shareAffinity)
+            {
+                switch (heroDevotion)
+                {
+                    case DevotionLevel.Fanatic:
+                        switch (enemyDevotion)
+                        {
+                            case DevotionLevel.Fanatic:
+                                value += -50;
+                                break;
+                            case DevotionLevel.Follower:
+                            case DevotionLevel.Devoted:
+                                value += -20;
+                                break;
+                        }
+
+                        break;
+                    case DevotionLevel.Devoted:
+                        if (enemyDevotion== DevotionLevel.Fanatic)
+                            value += -30;
+                        else
+                            value += -15;
+                        break;
+                    case DevotionLevel.Follower:
+                        switch (enemyDevotion)
+                        {
+                            case DevotionLevel.Fanatic:
+                                value += -30;
+                                break;
+                            case DevotionLevel.Follower:
+                            case DevotionLevel.Devoted:
+                                value += -20;
+                                break;
+                        }
+
+                        break;
+                    case DevotionLevel.None:
+                        break;
+                }
+            }
+            else   // hostile and neutral gods.
+            {
+                switch (heroDevotion)
+                {
+                    case DevotionLevel.Fanatic:
+                        if (enemyDevotion==DevotionLevel.Fanatic)
+                        {
+                            value += -100;
+                        }
+                        break;
+                    case DevotionLevel.Devoted:
+                        if (enemyDevotion== DevotionLevel.Fanatic)
+                            value += -50;
+                        else
+                            value += -25;
+                        break;
+                    case DevotionLevel.Follower:
+                        switch (enemyDevotion)
+                        {
+                            case DevotionLevel.Fanatic:
+                                value += -30;
+                                break;
+                            case DevotionLevel.Follower:
+                            case DevotionLevel.Devoted:
+                                value += -20;
+                                break;
+                        } ;
+                        break;
+                    case DevotionLevel.None:
+                        break;
+                }
+            }
+
+            if (enemyDevotion == DevotionLevel.None) return value;
+            
+            if (religion.HostileReligions.Contains(comparedToReligion))
+            {
+                value += -25;       //additional base value if the god is hostile, irrespective about the state of religion.
+            }
+
+            return value;
         }
     }
 }
