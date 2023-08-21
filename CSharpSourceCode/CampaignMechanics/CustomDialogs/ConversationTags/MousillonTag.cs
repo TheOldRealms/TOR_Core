fb3601c196cf18f2fc19fcec7d720ff1d4c3707a
Linus Tiemann <43908801+Z3rca@users.noreply.github.com>
2023-08-21 23:31:42 +0200

    Text for Lords (Bretonnia, Mousillon)
    fixed one perk in damsel perks
    Added a mousillonTag for conversationstags
    fixed spelling mistake in clan

---
+++ CSharpSourceCode/CampaignMechanics/CustomDialogs/ConversationTags/MousillonTag.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TaleWorlds.CampaignSystem.Conversation.Tags
+{
+    public class MousillonTag : ConversationTag
+    {
+        public const string Id = "MousillonTag";
+        public override string StringId => nameof (MousillonTag);
+        public override bool IsApplicableTo(CharacterObject character)
+        {
+            return character.Culture.StringId == "mousillon";
+        }
+    }
+}
--- CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/GrailDamselCareerChoices.cs
+++ CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/GrailDamselCareerChoices.cs
@@ -93,7 +93,7 @@ namespace TOR_Core.CharacterDevelopment.CareerSystem.Choices
             _inspirationOfTheLadyPassive1.Initialize(CareerID, "25% extra chance to recruit another unit for free", "InspirationOfTheLady", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(25, PassiveEffectType.Special, true)); 
             _inspirationOfTheLadyPassive2.Initialize(CareerID, "Wounded troops in your party heal faster.", "InspirationOfTheLady", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(2, PassiveEffectType.TroopRegeneration));
             _inspirationOfTheLadyPassive3.Initialize(CareerID, "All Knight troops wages are reduced by 25%", "InspirationOfTheLady", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(25, PassiveEffectType.Special, true));
-            _inspirationOfTheLadyPassive4.Initialize(CareerID, "15% Wardsave if your armor weight is not exceeding 4 weight", "InspirationOfTheLady", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(15, PassiveEffectType.Special, true));
+            _inspirationOfTheLadyPassive4.Initialize(CareerID, "10% Wardsave if your armor weight is not exceeding 4 weight", "InspirationOfTheLady", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(10, PassiveEffectType.Special, true));
 
             _talesOfGilesPassive1.Initialize(CareerID, "Increases Hitpoints by 50.", "TalesOfGiles", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(50, PassiveEffectType.Health));
             _talesOfGilesPassive2.Initialize(CareerID, "Bretonnian Units receive 10% Wardsave", "TalesOfGiles", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(10, PassiveEffectType.Special, true));
@@ -107,7 +107,7 @@ namespace TOR_Core.CharacterDevelopment.CareerSystem.Choices
             
             _justCausePassive1.Initialize(CareerID, "Upgrade costs are halfed", "JustCause", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(-0.5f, PassiveEffectType.TroopUpgradeCost));
             _justCausePassive2.Initialize(CareerID, "100 XP every day for non-knightly units in party", "JustCause", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(100, PassiveEffectType.Special, false)); //CareerPerkCampaign Behavior 101
-            _justCausePassive3.Initialize(CareerID, "+15% Wardsave if your armor weight is not exceeding 4 weight", "JustCause", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(15, PassiveEffectType.Special, true));
+            _justCausePassive3.Initialize(CareerID, "+20% Wardsave if your armor weight is not exceeding 4 weight", "JustCause", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(15, PassiveEffectType.Special, true));
             _justCausePassive4.Initialize(CareerID, "Increases positive relationship gains by 20%", "JustCause", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(20, PassiveEffectType.Special, true)); //TorDiplomacy model 23
             
             _secretsOFTheGrailPassive1.Initialize(CareerID, "Extra Lightning spell damage(30%).", "SecretsOfTheGrail", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(PassiveEffectType.Damage, new DamageProportionTuple(DamageType.Lightning, 30), AttackTypeMask.Spell));
