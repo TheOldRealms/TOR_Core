d153b52be90687af748a68ccd3a361ac85655a77
Péter Haraszti <hunharibo@gmail.com>
2023-10-01 19:03:42 +0200

    fix caravan master crash

--- CSharpSourceCode/HarmonyPatches/CaravanMasterPatch.cs
+++ CSharpSourceCode/HarmonyPatches/CaravanMasterPatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Party.PartyComponents;
@@ -12,9 +13,8 @@ namespace TOR_Core.HarmonyPatches
     {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(CaravanPartyComponent), "InitializeCaravanOnCreation")]
-        public static bool CaravanLeaderOverride(MobileParty mobileParty, Hero caravanLeader, ItemRoster caravanItems, int troopToBeGiven, CaravanPartyComponent __instance)
+        public static bool CaravanLeaderOverride(MobileParty mobileParty, Hero caravanLeader, ItemRoster caravanItems, int troopToBeGiven, bool isElite, CaravanPartyComponent __instance)
         {
-            /*
             __instance.MobileParty.Aggressiveness = 0f;
 
             if (troopToBeGiven == 0)
@@ -35,17 +35,19 @@ namespace TOR_Core.HarmonyPatches
                 }
                 troopToBeGiven = num2;
             }
-            mobileParty.InitializeMobilePartyAtPosition(__instance.Settlement.Culture.CaravanPartyTemplate, __instance.Settlement.GatePosition, troopToBeGiven);
+            PartyTemplateObject pt = isElite ? __instance.Settlement.Culture.EliteCaravanPartyTemplate : __instance.Settlement.Culture.CaravanPartyTemplate;
+            mobileParty.InitializeMobilePartyAtPosition(pt, __instance.Settlement.GatePosition, troopToBeGiven);
             if (caravanLeader != null)
             {
                 mobileParty.MemberRoster.AddToCounts(caravanLeader.CharacterObject, 1, true, 0, 0, true, -1);
             }
             else
             {
-                CharacterObject character2 = __instance.Settlement.Culture.CaravanMaster;
-                mobileParty.MemberRoster.AddToCounts(character2, 1, true, 0, 0, true, -1);
+                CharacterObject character = GetCaravanMaster(__instance.Settlement.Culture);
+                mobileParty.MemberRoster.AddToCounts(character, 1, true, 0, 0, true, -1);
             }
-            mobileParty.Party.Visuals.SetMapIconAsDirty();
+            mobileParty.ActualClan = __instance.Owner.Clan;
+            mobileParty.Party.SetVisualAsDirty();
             mobileParty.InitializePartyTrade(10000 + ((__instance.Owner.Clan == Clan.PlayerClan) ? 5000 : 0));
             if (caravanItems != null)
             {
@@ -67,8 +69,32 @@ namespace TOR_Core.HarmonyPatches
                 mobileParty.ItemRoster.Add(new ItemRosterElement(itemObject, (int)(mobileParty.MemberRoster.TotalManCount * 0.5f), null));
             }
             return false;
-            */
-            return true;
+        }
+
+        private static CharacterObject GetCaravanMaster(CultureObject culture)
+        {
+            CharacterObject master = null;
+            if(culture == null)
+            {
+                master = CharacterObject.All.FirstOrDefault(x => x.Occupation == Occupation.CaravanGuard);
+            }
+            else
+            {
+                master = culture.CaravanMaster;
+                if(master == null)
+                {
+                    master = CharacterObject.All.FirstOrDefault(x => x.Occupation == Occupation.CaravanGuard && x.IsInfantry && x.Level == 26 && x.Culture == culture);
+                }
+                if (master == null)
+                {
+                    master = CharacterObject.All.FirstOrDefault(x => x.Occupation == Occupation.CaravanGuard && x.Culture == culture);
+                }
+                if (master == null)
+                {
+                    master = CharacterObject.All.FirstOrDefault(x => x.Occupation == Occupation.CaravanGuard);
+                }
+            }
+            return master;
         }
     }
 }
