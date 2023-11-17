fb09465cd078a04fa285f1ee834f252b7e74b894
Jasper Zhang <jzz111@gmail.com>
2023-11-17 14:40:06 -0500

    Initial marriage model to inherit core ruleset from Taleworlds

--- CSharpSourceCode/Models/TORMarriageModel.cs
+++ CSharpSourceCode/Models/TORMarriageModel.cs
@@ -7,12 +7,44 @@ namespace TOR_Core.Models
     {
         public override bool IsCoupleSuitableForMarriage(Hero firstHero, Hero secondHero)
         {
-            return false;
+            var baseDecision = base.IsCoupleSuitableForMarriage(firstHero, secondHero);
+
+            var finalDecision = baseDecision && 
+                GeneralTorMarriageRuleSet(firstHero, secondHero);
+
+            return finalDecision;
+
+            //return firstHero.IsFemale != secondHero.IsFemale && 
+            //    !firstHero.IsFactionLeader && !secondHero.IsFactionLeader &&
+            //    firstHero.IsLord && secondHero.IsLord;
         }
 
         public override bool IsSuitableForMarriage(Hero maidenOrSuitor)
         {
-            return false;
+            var baseDecision = base.IsSuitableForMarriage(maidenOrSuitor);
+
+            var finalDecision = baseDecision && 
+                GeneralTorMarriageRuleSet(CharacterObject.PlayerCharacter.HeroObject, maidenOrSuitor) &&
+                HeroSpecificTorMarriageRuleSet(maidenOrSuitor);
+
+            return finalDecision;
+
+            //return CharacterObject.PlayerCharacter.IsFemale != maidenOrSuitor.IsFemale &&
+            //    CharacterObject.PlayerCharacter.HeroObject.MapFaction.Id == maidenOrSuitor.MapFaction.Id &&
+            //    !CharacterObject.PlayerCharacter.HeroObject.IsFactionLeader && !maidenOrSuitor.IsFactionLeader &&
+            //    maidenOrSuitor.IsLord;
+        }
+
+        protected bool GeneralTorMarriageRuleSet(Hero firstHero, Hero secondHero)
+        {
+            // New Rules
+            return true;
+        }
+
+        protected bool HeroSpecificTorMarriageRuleSet(Hero maidenOrSuitor)
+        {
+            // New Rules
+            return true;
         }
     }
 }
