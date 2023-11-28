d857f4a20bbcfc3e1620e737a5521d6acdddeab7
Péter Haraszti <hunharibo@gmail.com>
2023-11-28 20:48:55 +0100

    add example button

---
+++ CSharpSourceCode/Extensions/UI/PartyCharacterVMExtension.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia.Pages;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Party;
+using TaleWorlds.Core.ViewModelCollection.Information;
+using TaleWorlds.Library;
+using TOR_Core.Utilities;
+
+namespace TOR_Core.Extensions.UI
+{
+    [ViewModelExtension(typeof(PartyCharacterVM))]
+    public class PartyCharacterVMExtension : BaseViewModelExtension
+    {
+        private bool _shouldButtonBeVisible;
+        private bool _isButtonEnabled;
+        private BasicTooltipViewModel _buttonHint;
+
+        public PartyCharacterVMExtension(ViewModel vm) : base(vm)
+        {
+            _buttonHint = new BasicTooltipViewModel(GetButtonHintText);
+            RefreshValues();
+        }
+
+        public override void RefreshValues()
+        {
+            //Update datasource properties here. Gets called every time the base ViewModel would get refreshed.
+            //Careful to always update the property, not the field behind it directly, because then the engine won't get notified and events won't be raised.
+            ShouldButtonBeVisible = true;
+            IsButtonEnabled = true;
+
+        }
+
+        private string GetButtonHintText()
+        {
+            //This method will get called every time the hint shows up, so you can dynamically construct the hint text based on context, not just static text
+            return "This is Z3rca's button. This text will show up as hint.";
+        }
+
+        public void ExecuteButtonClick()
+        {
+            TORCommon.Say("Button clicked.");
+        }
+
+        [DataSourceProperty]
+        public bool ShouldButtonBeVisible
+        {
+            get
+            {
+                return _shouldButtonBeVisible;
+            }
+            set
+            {
+                if (value != _shouldButtonBeVisible)
+                {
+                    _shouldButtonBeVisible = value;
+                    _vm.OnPropertyChangedWithValue(value, "ShouldButtonBeVisible");
+                }
+            }
+        }
+
+        [DataSourceProperty]
+        public bool IsButtonEnabled
+        {
+            get
+            {
+                return _isButtonEnabled;
+            }
+            set
+            {
+                if (value != _isButtonEnabled)
+                {
+                    _isButtonEnabled = value;
+                    _vm.OnPropertyChangedWithValue(value, "IsButtonEnabled");
+                }
+            }
+        }
+
+        [DataSourceProperty]
+        public BasicTooltipViewModel ButtonHint
+        {
+            get
+            {
+                return _buttonHint;
+            }
+            set
+            {
+                if (value != _buttonHint)
+                {
+                    _buttonHint = value;
+                    _vm.OnPropertyChangedWithValue(value, "ButtonHint");
+                }
+            }
+        }
+    }
+}
