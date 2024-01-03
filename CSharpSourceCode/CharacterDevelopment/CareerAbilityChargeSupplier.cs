9a14a7a355ce60edd76bc38fe4e134cd7aeee46f
Linus Tiemann <linus-tiemann@live.de>
2024-01-03 19:50:21 +0100

    clean up

--- CSharpSourceCode/CharacterDevelopment/CareerAbilityChargeSupplier.cs
+++ CSharpSourceCode/CharacterDevelopment/CareerAbilityChargeSupplier.cs
@@ -10,23 +10,19 @@ namespace TOR_Core.CharacterDevelopment
 {
     public static class CareerAbilityChargeSupplier
     {
-        public static float WitchHunterCareerCharge( Agent affectingAgent,Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
+        public static float WitchHunterCareerCharge(Agent affectingAgent, Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
         {
-            
             if (chargeType != ChargeType.DamageDone) return 0;
             if (!affectingAgent.BelongsToMainParty()) return 0;
-            
+
             ExplainedNumber explainedNumber = new ExplainedNumber(0);
-            
+
             if (affectingAgent.IsHero && affectingAgent.IsMainAgent)
             {
-                
                 explainedNumber.Add(chargeValue);
             }
-            
-            
-            
-            if ( mask == AttackTypeMask.Ranged || mask == AttackTypeMask.Melee && Hero.MainHero.HasCareerChoice("HuntTheWickedKeystone") )
+
+            if (mask == AttackTypeMask.Ranged || mask == AttackTypeMask.Melee && Hero.MainHero.HasCareerChoice("HuntTheWickedKeystone"))
             {
                 if (mask == AttackTypeMask.Ranged)
                 {
@@ -43,57 +39,54 @@ namespace TOR_Core.CharacterDevelopment
 
             return explainedNumber.ResultNumber;
         }
-        
-        public static float NecromancerCareerCharge(Agent affectingAgent,Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
+
+        public static float NecromancerCareerCharge(Agent affectingAgent, Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
         {
             if (!affectingAgent.BelongsToMainParty()) return 0;
-            if(mask== AttackTypeMask.Ranged) return 0;
-            
-            if( mask == AttackTypeMask.Melee&&!affectingAgent.IsUndead())
+            if (mask == AttackTypeMask.Ranged) return 0;
+
+            if (mask == AttackTypeMask.Melee && !affectingAgent.IsUndead())
             {
                 return 0;
             }
-            
-            if ((chargeType != ChargeType.DamageDone && chargeType != ChargeType.Healed)) return 0; 
-            
-            ExplainedNumber explainedNumber =new ExplainedNumber();
-            
+
+            if (( chargeType != ChargeType.DamageDone && chargeType != ChargeType.Healed )) return 0;
+
+            ExplainedNumber explainedNumber = new ExplainedNumber();
+
             explainedNumber.Add(chargeValue);
-            
-            
+
             if (mask == AttackTypeMask.Spell)
             {
                 explainedNumber.AddFactor(-0.9f);
             }
-                        
+
             explainedNumber.Add(chargeValue);
             return explainedNumber.ResultNumber;
         }
-        
-        public static float GrailDamselCareerCharge(Agent affectingAgent,Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
+
+        public static float GrailDamselCareerCharge(Agent affectingAgent, Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
         {
             if (chargeType != ChargeType.DamageDone || chargeType != ChargeType.Healed) return 0;
 
             if (mask != AttackTypeMask.Spell) return 0;
 
             if (!affectingAgent.IsHero) return 0;
-            
-            
-            ExplainedNumber explainedNumber =new ExplainedNumber();
-            
-          
+
+
+            ExplainedNumber explainedNumber = new ExplainedNumber();
 
             if (affectingAgent.GetOriginMobileParty() != MobileParty.MainParty) return 0;
-           
+
 
             if (!affectingAgent.IsMainAgent && !Hero.MainHero.HasCareerChoice("InspirationOfTheLadyKeystone")) return 0;
-            
+
             explainedNumber.Add(chargeValue);
-                       
+
             explainedNumber.AddFactor(-0.9f);
 
             var choices = Hero.MainHero.GetAllCareerChoices();
-                            
+
             if (choices.Contains("VividVisionsKeystone"))
             {
                 var choice = TORCareerChoices.GetChoice("VividVisionsKeystone");
@@ -103,36 +96,33 @@ namespace TOR_Core.CharacterDevelopment
                     explainedNumber.AddFactor(value);
                 }
             }
+
             if (choices.Contains("InspirationOfTheLadyKeystone"))
             {
                 var choice = TORCareerChoices.GetChoice("InspirationOfTheLadyKeystone");
                 if (choice != null)
                 {
-                    explainedNumber.AddFactor(-0.05f);           // Originally only 10% of charge is taken into account, now it would be 5% 
+                    explainedNumber.AddFactor(-0.05f); // Originally only 10% of charge is taken into account, now it would be 5% 
                 }
             }
+
             return explainedNumber.ResultNumber;
-
-
-
-
         }
 
-        public static float MinorVampireCareerCharge(Agent affectingAgent,Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
+        public static float MinorVampireCareerCharge(Agent affectingAgent, Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
         {
             if (affectingAgent.GetOriginMobileParty() != MobileParty.MainParty) return 0;
-            if (chargeType != ChargeType.DamageDone) return 0; 
-            
-            ExplainedNumber explainedNumber =new ExplainedNumber();
-           
-            
+            if (chargeType != ChargeType.DamageDone) return 0;
+
+            ExplainedNumber explainedNumber = new ExplainedNumber();
+
             explainedNumber.Add(chargeValue);
 
             if (mask == AttackTypeMask.Spell && Hero.MainHero.GetAllCareerChoices().Contains("ArkayneKeystone"))
             {
                 explainedNumber.AddFactor(-0.9f);
             }
-                            
+
             if (Hero.MainHero.GetAllCareerChoices().Contains("NewBloodKeystone"))
             {
                 var choice = TORCareerChoices.GetChoice("NewBloodKeystone");
@@ -156,21 +146,19 @@ namespace TOR_Core.CharacterDevelopment
             return explainedNumber.ResultNumber;
         }
 
-        public static float BloodKnightCareerCharge(Agent affectingAgent,Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
+        public static float BloodKnightCareerCharge(Agent affectingAgent, Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
         {
-            
             if (chargeType != ChargeType.NumberOfKills) return 0;
 
             if (!affectingAgent.IsHero) return 0;
-            
+
             ExplainedNumber explainedNumber = new ExplainedNumber();
-            
-            if (affectingAgent.IsMainAgent || affectingAgent.GetOriginMobileParty().IsMainParty&&Hero.MainHero.HasCareerChoice("NightRiderKeystone"))
+
+            if (affectingAgent.IsMainAgent || affectingAgent.GetOriginMobileParty().IsMainParty && Hero.MainHero.HasCareerChoice("NightRiderKeystone"))
             {
                 explainedNumber.Add(chargeValue);
             }
-            
-            
+
             if (Hero.MainHero.HasCareerChoice("DreadKnightKeystone"))
             {
                 var choice = TORCareerChoices.GetChoice("DreadKnightKeystone");
@@ -180,29 +168,26 @@ namespace TOR_Core.CharacterDevelopment
                     explainedNumber.AddFactor(value);
                 }
             }
-            
+
             return explainedNumber.ResultNumber;
         }
-        
-        public static float WarriorPriestCareerCharge(Agent affectingAgent,Agent affectedAgent,ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
+
+        public static float WarriorPriestCareerCharge(Agent affectingAgent, Agent affectedAgent, ChargeType chargeType, int chargeValue, AttackTypeMask mask = AttackTypeMask.Melee, CareerHelper.ChargeCollisionFlag collisionFlag = CareerHelper.ChargeCollisionFlag.None)
         {
-            if(mask!=AttackTypeMask.Melee ) return 0;
+            if (mask != AttackTypeMask.Melee) return 0;
             if (chargeType == ChargeType.DamageTaken && ( !affectingAgent.IsMainAgent || !Hero.MainHero.HasCareerChoice("BookOfSigmarKeystone") )) return 0;
-            
-            ExplainedNumber explainedNumber =new ExplainedNumber();
+
+            ExplainedNumber explainedNumber = new ExplainedNumber();
             explainedNumber.Add(chargeValue);
-            
-            explainedNumber.Add((float) chargeValue / Hero.MainHero.MaxHitPoints);      //proportion of lost health 
-                        
+
+            explainedNumber.Add((float)chargeValue / Hero.MainHero.MaxHitPoints); //proportion of lost health 
+
             if (collisionFlag == CareerHelper.ChargeCollisionFlag.HitShield)
             {
                 explainedNumber.AddFactor(-0.85f);
             }
 
             return explainedNumber.ResultNumber;
-
         }
-        
-        
     }
 }
