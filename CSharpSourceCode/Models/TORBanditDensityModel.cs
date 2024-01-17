7d2be13aa51a6c50e8897dc0e6f0867b73b114ba
Jasper Zhang <jzz111@gmail.com>
2024-01-17 00:17:54 -0500

    Made campaign range configurable

--- CSharpSourceCode/Models/TORBanditDensityModel.cs
+++ CSharpSourceCode/Models/TORBanditDensityModel.cs
@@ -6,22 +6,17 @@ namespace TOR_Core.Models
 {
     public class TORBanditDensityModel : DefaultBanditDensityModel
     {
-        // Usually it takes about 70 days or so to get yourself on your foot... very player style dependent
-        public const int EARLY_GAME_END_YEAR = 1;
-        // Usually late game means starting your own kingdom and that usually happens when the player is in their 30s, 10yrs put the player right in that range
-        public const int MID_GAME_END_YEAR = 10;
-
         // 21 days per season, 4 seasons
         public const int DAYS_IN_YEAR = 84;
         public override int NumberOfMaximumLooterParties
         {
             get
             {
-                if (Campaign.Current.CampaignStartTime.ElapsedDaysUntilNow < EARLY_GAME_END_YEAR * DAYS_IN_YEAR)
+                if (Campaign.Current.CampaignStartTime.ElapsedDaysUntilNow < TORConfig.YearsToEndEarlyCampaign * DAYS_IN_YEAR)
                 {
                     return TORConfig.NumberOfMaximumLooterPartiesEarly;
                 }
-                else if(Campaign.Current.CampaignStartTime.ElapsedDaysUntilNow < MID_GAME_END_YEAR * DAYS_IN_YEAR)
+                else if(Campaign.Current.CampaignStartTime.ElapsedDaysUntilNow < TORConfig.YearsToEndMidCampaign * DAYS_IN_YEAR)
                 {
                     return TORConfig.NumberOfMaximumLooterParties;
                 }
--- CSharpSourceCode/Utilities/TORConfig.cs
+++ CSharpSourceCode/Utilities/TORConfig.cs
@@ -18,6 +18,8 @@ namespace TOR_Core.Utilities
         public static int FakeBannerFrequency => _config.FakeBannerFrequency;
         public static int MaximumCustomResourceValue => _config.MaximumCustomResourceValue;
         public static int NumberOfTroopsPerFormationWithStandard => _config.NumberOfTroopsPerFormationWithStandard;
+        public static int YearsToEndEarlyCampaign => _config.YearsToEndEarlyCampaign;
+        public static int YearsToEndMidCampaign => _config.YearsToEndMidCampaign;
         public static int NumberOfMaximumLooterPartiesEarly => _config.NumberOfMaximumLooterPartiesEarly;
         public static int NumberOfMaximumLooterParties => _config.NumberOfMaximumLooterParties;
         public static int NumberOfMaximumLooterPartiesLate => _config.NumberOfMaximumLooterPartiesLate;
@@ -53,6 +55,10 @@ namespace TOR_Core.Utilities
             public int MaximumCustomResourceValue;
             [XmlAttribute]
             public int NumberOfTroopsPerFormationWithStandard;
+            [XmlAttribute]
+            public int YearsToEndEarlyCampaign;
+            [XmlAttribute]
+            public int YearsToEndMidCampaign;
             [XmlAttribute]
             public int NumberOfMaximumLooterPartiesEarly;
             [XmlAttribute]
