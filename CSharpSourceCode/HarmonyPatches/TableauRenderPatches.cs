08b0bb96117fb326421fd54f7132bc552c228cae
Péter Haraszti <hunharibo@gmail.com>
2024-01-20 14:07:21 +0100

    change map conversation camera to match speaker's eye height

---
+++ CSharpSourceCode/HarmonyPatches/TableauRenderPatches.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using SandBox.View.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Engine;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade.View;
+
+namespace TOR_Core.HarmonyPatches
+{
+    [HarmonyPatch]
+    public static class TableauRenderPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MapConversationTableau), "FirstTimeInit")]
+        public static void PrefixMapConversationRender(ref Camera ____continuousRenderCamera, List<AgentVisuals> ____agentVisuals)
+        {
+            if(____continuousRenderCamera != null && ____agentVisuals != null && ____agentVisuals.Count > 0)
+            {
+                var eyePos = ____agentVisuals[0].GetGlobalStableEyePoint(true);
+                var cameraFrame = ____continuousRenderCamera.Frame;
+                cameraFrame.origin.z = eyePos.z - 0.15f;
+                ____continuousRenderCamera.Frame = cameraFrame;
+            }
+        }
+    }
+}
