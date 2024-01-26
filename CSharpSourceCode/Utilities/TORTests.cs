ebb501a301af68f5c47f18d384e6554fcc56767f
Péter Haraszti <hunharibo@gmail.com>
2024-01-26 21:11:25 +0100

    add native crash test

---
+++ CSharpSourceCode/Utilities/TORTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Windows.Forms;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace TOR_Core.Utilities
+{
+    public class TORTests
+    {
+        private static TORTests _instance;
+        public static TORTests Instance 
+        {
+            get
+            {
+                if( _instance == null ) _instance = new TORTests();
+                return _instance;
+            }
+        }
+
+
+        [HandleProcessCorruptedStateExceptions]
+        [SecurityCritical]
+        public void TriggerCorruptedMemoryStateException()
+        {
+            try
+            {
+                var ptr = new IntPtr(42);
+                Marshal.StructureToPtr(42, ptr, true);
+            }
+            catch (Exception e)
+            {
+                TORCommon.Log(e.ToString(), NLog.LogLevel.Error);
+                Process.GetCurrentProcess().Kill();
+            }
+        }
+    }
+}
