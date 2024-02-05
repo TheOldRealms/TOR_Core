35cf5ad7c5f0d32ff8dca7a9bf8106e897398225
Linus Tiemann <linus-tiemann@live.de>
2024-02-05 01:23:20 +0100

    made perk work for career ability

--- CSharpSourceCode/AbilitySystem/Scripts/NetherballScript.cs
+++ CSharpSourceCode/AbilitySystem/Scripts/NetherballScript.cs
@@ -2,6 +2,7 @@
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TOR_Core.BattleMechanics.TriggeredEffect;
 using TOR_Core.CharacterDevelopment;
 using TOR_Core.Extensions;
 using TOR_Core.Utilities;
@@ -13,40 +14,33 @@ namespace TOR_Core.AbilitySystem.Scripts
         private bool _init;
         private float _radius = 0f;
 
-        protected override void OnAfterTick(float dt)
+        protected override void OnInit()
         {
-            if (!_init)
-            {
-                _radius = Ability.Template.Radius;
-                var template = Ability.Template.AssociatedTriggeredEffectTemplates.FirstOrDefault();
-                _radius = template.Radius;
-            }
-            
-        }
+            base.OnInit();
+            var _effects = this.GetEffectsToTrigger();
+                var radius = 0f;
+                foreach (var effect in _effects)
+                {
+                    if (effect.EffectRadius > radius)
+                    {
+                        radius = effect.EffectRadius;
+                    }
+                }
 
-        protected override void OnBeforeRemoved(int removeReason)
-        {
-            base.OnBeforeRemoved(removeReason);
-            var targets = this.ExplicitTargetAgents;
-
-            if (targets != null)
-            {
-                TORCommon.Say(targets.Count+"");
-            }
+                _radius = radius;
+                TORCommon.Say(_radius + " radius");
         }
+        
 
         protected override void HandleCollision(Vec3 position, Vec3 normal)
         {
-            base.HandleCollision(position, normal);
-            
-           
-            
             if(Hero.MainHero.HasCareerChoice("HungerForKnowledgeKeystone"))
             { 
                 var agents = Mission.Current.GetNearbyEnemyAgents(position.AsVec2, _radius, CasterAgent.Team, new MBList<Agent>());
-                Hero.MainHero.AddWindsOfMagic(agents.Count);
+                Hero.MainHero.AddCustomResource("DarkEnergy", agents.Count);
+                TORCommon.Say(agents.Count+ " got added radius "+ _radius);
             }
-
+            base.HandleCollision(position, normal);
            
         }
     }
