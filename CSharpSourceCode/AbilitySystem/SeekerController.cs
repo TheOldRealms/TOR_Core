fec4b3f5aef8b7c5fb15a007ee8805c0988461f1
Jasper Zhang <jzz111@gmail.com>
2024-02-18 21:00:46 -0500

    Diplomacy to declare war based on distance more (#73)
    * Separated distance factor from religion factor in TOR aggression algorithm. Refactored aggression into a diplomacy subsystem
    
    * Decoupled code to make it easier to maintain
    
    * Discovered distance score is reverse of what we want... flipped it and multiplied by 2...
    
    * OK, gotta tone down the warmongering here... the wars are getting out of hand...
    
    * Include bin for previous commit

--- CSharpSourceCode/AbilitySystem/SeekerController.cs
+++ CSharpSourceCode/AbilitySystem/SeekerController.cs
@@ -23,7 +23,7 @@ namespace TOR_Core.AbilitySystem
 
         public MatrixFrame CalculateRotatedFrame(MatrixFrame globalFrame, float dt)
         {
-            if (enabled && _target != null && (_target.Agent != null || _target.Formation.CountOfUnits > 0))
+            if (enabled && _target != null && (_target.Agent != null || (_target.Formation != null && _target.Formation.CountOfUnits > 0)))
             {
                 var particleDirection = globalFrame.origin + globalFrame.rotation.f.NormalizedCopy();
                 var error = _target.GetPosition() - particleDirection;
