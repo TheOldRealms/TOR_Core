7c70caa2afa1a3367eb551df7980897f00b4693d
Linus Tiemann <43908801+Z3rca@users.noreply.github.com>
2024-04-02 21:31:07 +0200

    cleanup blast of agony script

--- CSharpSourceCode/AbilitySystem/Scripts/BlastOfAgonyScript.cs
+++ CSharpSourceCode/AbilitySystem/Scripts/BlastOfAgonyScript.cs
@@ -1,9 +1,6 @@
-using System.Linq;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
-using TOR_Core.BattleMechanics.TriggeredEffect;
-using TOR_Core.CharacterDevelopment;
 using TOR_Core.Extensions;
 using TOR_Core.Utilities;
 
@@ -11,42 +8,36 @@ namespace TOR_Core.AbilitySystem.Scripts
 {
     public class BlastOfAgonyScript : CareerAbilityMissleScript
     {
-        private bool _init;
-        private float _radius = 0f;
+        private float _radius;
 
         protected override void OnInit()
         {
             base.OnInit();
-            var _effects = this.GetEffectsToTrigger();
-                var radius = 0f;
-                foreach (var effect in _effects)
-                {
-                    if (effect.EffectRadius > radius)
-                    {
-                        radius = effect.EffectRadius;
-                    }
-                }
-
-                _radius = radius;
-                TORCommon.Say(_radius + " radius");
+            var _effects = GetEffectsToTrigger();
+            var radius = 0f;
+            foreach (var effect in _effects)
+            {
+                if (effect.EffectRadius > radius)
+                    radius = effect.EffectRadius;
+            }
+            
+            _radius = radius;
         }
         
-
         protected override void HandleCollision(Vec3 position, Vec3 normal)
         {
             if (Agent.Main == null)
                 return;
-            if(Hero.MainHero.HasCareerChoice("WitchSightKeystone"))
-            { 
+            if (Hero.MainHero.HasCareerChoice("WitchSightKeystone"))
+            {
                 var agents = Mission.Current.GetNearbyEnemyAgents(position.AsVec2, _radius, CasterAgent.Team, new MBList<Agent>());
                 var maxWindsOfMagic = Hero.MainHero.GetExtendedInfo().MaxWindsOfMagic;
 
                 var value = maxWindsOfMagic * 0.025f;
                 Hero.MainHero.AddCustomResource("WindsOfMagic", value);
-                TORCommon.Say(agents.Count+ " got added radius "+ _radius);
             }
+
             base.HandleCollision(position, normal);
-           
         }
     }
 }
