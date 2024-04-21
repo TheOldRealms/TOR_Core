5d29083527edd04c67ff339506afcc18fc3319a7
Linus Tiemann <linus-tiemann@live.de>
2024-04-21 10:46:50 +0200

    Encyclopedia Unit Page info and cleanup
    also moved Attribute description out

---
+++ CSharpSourceCode/Extensions/UI/UnitEncyclopediaVMExtension.cs
@@ -0,0 +1,61 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.ViewModelCollection.Encyclopedia.Pages;
+using TaleWorlds.Core;
+using TaleWorlds.Core.ViewModelCollection.Information;
+using TaleWorlds.Library;
+
+namespace TOR_Core.Extensions.UI
+{
+    [ViewModelExtension(typeof(EncyclopediaUnitPageVM))]
+    public class UnitEncyclopediaVMExtension : BaseViewModelExtension
+    {
+        private CharacterObject _characterObject;
+        private BasicTooltipViewModel _extendedInfoHint;
+
+        public UnitEncyclopediaVMExtension(ViewModel vm) : base(vm)
+        {
+            
+            RefreshValues();
+        }
+
+
+        public override void RefreshValues()
+        {
+            
+            var unitVM = _vm as EncyclopediaUnitPageVM;
+            if (_characterObject == null)
+            {
+                _characterObject = unitVM.Obj as CharacterObject;
+            }
+            
+            if(_characterObject==null) return;
+
+            var extendedInfoList = _characterObject.GetExtendedInfoToolTipText();
+            if (!extendedInfoList.IsEmpty())
+            {
+                ExtendedInfoHint = new BasicTooltipViewModel(()=> extendedInfoList);
+            }
+        }
+
+
+
+        public CharacterObject GetCharacterObject()
+        {
+            return _characterObject;
+        }
+        
+        
+        [DataSourceProperty]
+        public BasicTooltipViewModel ExtendedInfoHint
+        {
+            get => this._extendedInfoHint;
+            set
+            {
+                if (value == this._extendedInfoHint)
+                    return;
+                this._extendedInfoHint = value;
+                this._vm.OnPropertyChangedWithValue(nameof (ExtendedInfoHint));
+            }
+        }
+    }
+}
