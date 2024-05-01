79ec6bec60eca549701de35d12c7eff4036b164e
Linus Tiemann <linus-tiemann@live.de>
2024-05-01 08:33:27 +0200

    refactored code for ability script

--- CSharpSourceCode/AbilitySystem/Scripts/AxeOfUlricScript.cs
+++ CSharpSourceCode/AbilitySystem/Scripts/AxeOfUlricScript.cs
@@ -5,31 +5,23 @@ using TOR_Core.Extensions;
 
 namespace TOR_Core.AbilitySystem.Scripts
 {
-    public class AxeOfUlricScript: CareerAbilityScript
+    public class AxeOfUlricScript : CareerAbilityScript
     {
         protected override void OnInit()
         {
             base.OnInit();
 
-            if (Hero.MainHero.HasCareerChoice("FlameOfUlricKeystone"))
-            {
-                
-                if (Agent.Main.GetComponent<AbilityComponent>() != null)
-                {
-                    var abilities = Agent.Main.GetComponent<AbilityComponent>().KnownAbilitySystem;
-                    if (abilities.Count > 2)
-                    {
-                        var chosen = Agent.Main.GetComponent<AbilityComponent>().KnownAbilitySystem.TakeRandom(2).ToList();
-                        if(chosen[0].StringID == this.Ability.StringID)
-                            chosen[1].SetCoolDown(0);
-                        else
-                        {
-                            chosen[0].SetCoolDown(0);
-                        }
-                    }
-                }
+            if (!Hero.MainHero.HasCareerChoice("FlameOfUlricKeystone")) return;
+            var comp = Agent.Main.GetComponent<AbilityComponent>();
+            if (comp == null) return;
 
-            }
+            var abilities =
+                comp.KnownAbilitySystem.Where(x => x.StringID != Ability.StringID && x.GetCoolDownLeft() > 0);
+            var chosen = abilities.TakeRandom(0).FirstOrDefault();
+
+            if (chosen == null) return;
+            chosen.SetCoolDown(0);
         }
+
     }
 }
