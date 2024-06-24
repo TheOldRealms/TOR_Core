920f4394b284c451527167d07babf2fcd4beea6b
Linus Tiemann <linus-tiemann@live.de>
2024-06-24 22:26:56 +0200

    finish up root logic

--- CSharpSourceCode/CampaignMechanics/TORCustomSettlement/CustomSettlementMenus/OakOfAgesMenuLogic.cs
+++ CSharpSourceCode/CampaignMechanics/TORCustomSettlement/CustomSettlementMenus/OakOfAgesMenuLogic.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Helpers;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.Encounters;
 using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Core;
+using TaleWorlds.Library;
 using TaleWorlds.Localization;
 using TOR_Core.CampaignMechanics.CustomResources;
 using TOR_Core.Extensions;
@@ -16,9 +20,17 @@ public class OakOfAgesMenuLogic : TORBaseSettlementMenuLogic
     private const int PartySizeUpgradeCost = 100;
     private const int HealthUpgradeCost = 125;
     private const int GainUpgradeCost = 150;
-    
     private const int TroopUpkeepUpgradeCost = 200;
-    private const int TroopXpUpgradeCost = 175;
+
+    private const int TravelCost = 100;
+    private const int RootUnlockCost = 200;
+    private const int RootTravelCostReductionUpgradeCost = 500;
+    private const int RootTravelBackUpgradeCost = 750;
+    private const int RootHealUpgradeCost = 1000;
+    private Vec2 ArdenLocation = new Vec2(945.4077f, 1111.009f);
+    private Vec2 LaurelornLocation = new Vec2(1239.391f, 1276.938f);
+    private Vec2 GryphenWoodLocation = new Vec2(1606.698f, 1133.905f);
+    
     
     public static readonly List<string> PartyUpgradeAttributes =
     [
@@ -66,33 +78,6 @@ public class OakOfAgesMenuLogic : TORBaseSettlementMenuLogic
         AddOakOfAgeMenus(campaignGameStarter);
     }
     
-    
-    public void AddOakOfAgeMenus(CampaignGameStarter starter)
-    {
-        starter.AddGameMenu("oak_of_ages_menu", "{LOCATION_DESCRIPTION}", OakOfAgeMenuInit);
-        starter.AddGameMenuOption("oak_of_ages_menu", "branch", "Branches of the Oak",null, delegate
-        {
-            GameMenu.SwitchToMenu("oak_of_ages_branches_menu");
-        }, false, 4, false);
-        starter.AddGameMenuOption("oak_of_ages_menu", "leave", "{tor_custom_settlement_menu_leave_str}Leave...", delegate(MenuCallbackArgs args)
-        {
-            args.optionLeaveType = GameMenuOption.LeaveType.Leave;
-            return true;
-        }, (MenuCallbackArgs args) => PlayerEncounter.Finish(true), true);
-        
-        AddBranchesOfTheOakMenu(starter);
-       // AddWorldRootMenu(starter);
-    }
-
-    /*private void AddWorldRootMenu(CampaignGameStarter starter)
-    {
-        starter.AddGameMenu("oak_of_ages_branches_menu", "World roots", OakOfAgeMenuInit);
-        
-        starter.AddGameMenuOption("oak_of_ages_branches_menu", "rootMenu_AUnlock", "Unlock Pathway to the Forest of Arden. {PARTYSIZEUPGRADECOST}{FORESTHARMONY}",args => 
-            BranchUpgradeCondition(args,PartySizeUpgradeCost, "WEPartySizeUpgrade"),_ => UpgradeConsequence(PartySizeUpgradeCost,"WEPartySizeUpgrade" ));
-
-    }*/
-
     private void OakOfAgeMenuInit(MenuCallbackArgs args)
     {
         var settlement = Settlement.CurrentSettlement;
@@ -108,11 +93,161 @@ public class OakOfAgesMenuLogic : TORBaseSettlementMenuLogic
         MBTextManager.SetTextVariable("FORESTHARMONY1", CustomResourceManager.GetResourceObject("ForestHarmony").GetCustomResourceIconAsText());
 
     }
+    
+    
+    public void AddOakOfAgeMenus(CampaignGameStarter starter)
+    {
+        starter.AddGameMenu("oak_of_ages_menu", "{LOCATION_DESCRIPTION}", OakOfAgeMenuInit);
+        starter.AddGameMenuOption("oak_of_ages_menu", "branch", "Branches of the Oak",null, delegate
+        {
+            GameMenu.SwitchToMenu("oak_of_ages_branches_menu");
+        }, false, 4, false);
+        
+        starter.AddGameMenuOption("oak_of_ages_menu", "roots", "World roots of the Oak",null, delegate
+        {
+            GameMenu.SwitchToMenu("oak_of_ages_roots_menu");
+        }, false, 4, false);
+        
+        starter.AddGameMenuOption("oak_of_ages_menu", "leave", "{tor_custom_settlement_menu_leave_str}Leave...", delegate(MenuCallbackArgs args)
+        {
+            args.optionLeaveType = GameMenuOption.LeaveType.Leave;
+            return true;
+        }, (MenuCallbackArgs args) => PlayerEncounter.Finish(true), true);
+        
+        AddBranchesOfTheOakMenu(starter);
+        AddWorldRootMenu(starter);
+    }
+
+    private void AddWorldRootMenu(CampaignGameStarter starter)
+    {
+        starter.AddGameMenu("oak_of_ages_roots_menu", "World roots of the Oak", OakOfAgeMenuInit);
+        
+        MBTextManager.SetTextVariable("ROOTUNLOCKCOST",RootUnlockCost);
+        MBTextManager.SetTextVariable("ROOTTRAVELUPGRADE", RootTravelCostReductionUpgradeCost);
+        MBTextManager.SetTextVariable("ROOTRETURNUPGRADE",RootTravelBackUpgradeCost);
+        MBTextManager.SetTextVariable("ROOTHEALUPGRADE",RootHealUpgradeCost);
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_Unlock", "Unlock Pathway to the Forest of Arden. {ROOTUNLOCKCOST}{FORESTHARMONY}",args => 
+            RootUpgradeCondition(args,RootUnlockCost, "WorldRootTarget_Arden"),_ => RootUpgradeConsequence(RootUnlockCost,"WorldRootTarget_Arden" ));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_travel", "Travel to the forest of Arden. {WORLDROOTTRAVELCOST}{FORESTHARMONY}",args => 
+            RootAcessibleCondition(args, TravelCost, "WorldRootTarget_Arden"),_ => RootTravelConsequence(ArdenLocation));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_Unlock", "Unlock the pathway to Laurelorn. {ROOTUNLOCKCOST}{FORESTHARMONY}",args => 
+            RootUpgradeCondition(args,RootUnlockCost, "WorldRootTarget_Laurelorn"),_ => RootUpgradeConsequence(RootUnlockCost,"WorldRootTarget_Laurelorn" ));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_travel", "Travel to Laurelorn. {WORLDROOTTRAVELCOST}{FORESTHARMONY}",args => 
+            RootAcessibleCondition(args, TravelCost, "WorldRootTarget_Laurelorn"),_ => RootTravelConsequence(LaurelornLocation));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_Unlock", "Unlock the pathway to the Gryphenwood. {ROOTUNLOCKCOST}{FORESTHARMONY}",args => 
+            RootUpgradeCondition(args,RootUnlockCost, "WorldRootTarget_GryphenWood"),_ => RootUpgradeConsequence(RootUnlockCost,"WorldRootTarget_GryphenWood" ));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_travel", "Travel to the Gryphenwood. {WORLDROOTTRAVELCOST}{FORESTHARMONY}",args => 
+            RootAcessibleCondition(args, TravelCost, "WorldRootTarget_GryphenWood"),_ => RootTravelConsequence(GryphenWoodLocation));
+        
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_travel", "Reduce the travel cost. {ROOTTRAVELUPGRADE}{FORESTHARMONY}",
+            _ => Hero.MainHero.GetCultureSpecificCustomResourceValue()>=RootTravelCostReductionUpgradeCost,_ => RootUpgradeConsequence(RootTravelCostReductionUpgradeCost,"WETravelCostUpgrade"));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_travel", "Establish pathways back to the Oak of Ages. {ROOTRETURNUPGRADE}{FORESTHARMONY}",
+            args => RootUpgradeCondition(args, RootTravelBackUpgradeCost,"WETRavelBackUpgrade", true),_ => RootUpgradeConsequence(RootTravelBackUpgradeCost,"WETravelBackUpgrade"));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "rootMenu_A_travel", "Allow that all troops and heroes are healed upon using the world roots. {ROOTHEALUPGRADE}{FORESTHARMONY}",
+            args => RootUpgradeCondition(args, RootTravelBackUpgradeCost,"WETRavelHealUpgrade", true),_ => RootUpgradeConsequence(RootHealUpgradeCost,"WETravelHealUpgrade"));
+        
+        starter.AddGameMenuOption("oak_of_ages_roots_menu", "branchMenu_leave", "Leave...",
+            delegate(MenuCallbackArgs args)
+            {
+                args.optionLeaveType = GameMenuOption.LeaveType.Leave;
+                return true;
+            },
+            delegate (MenuCallbackArgs args)
+            {
+                GameMenu.SwitchToMenu("oak_of_ages_menu");
+            }, 
+            false, -1, false);
+    }
+
+    private bool RootUpgradeCondition(MenuCallbackArgs args, int upgradeCost, string upgradeId, bool showAnyway=false)
+    {
+        if (Hero.MainHero.HasAttribute(upgradeId)) return false;
+
+        if (Hero.MainHero.GetCultureSpecificCustomResourceValue() >= upgradeCost)
+        {
+            return true;
+        }
+        else
+        {
+            args.Tooltip = new TextObject("{=tor_custom_settlement_we_party_size_info_str}Not enough Harmony.");
+            args.IsEnabled = false;
+    
+            return true;
+        }
+    }
+    
+    private bool RootAcessibleCondition(MenuCallbackArgs args, int travelCost, string upgradeId)
+    {
+        var cost = travelCost;
+        if (!Hero.MainHero.HasAttribute(upgradeId)) return false;
+
+        if (Hero.MainHero.HasAttribute("WETravelCostUpgrade"))
+        {
+            cost /= 2;
+        }
+        
+        
+        if (Hero.MainHero.GetCultureSpecificCustomResourceValue() > cost)
+        {
+            return true;
+        }
+        else
+        {
+            args.Tooltip = new TextObject("{=tor_custom_settlement_we_party_size_info_str}Not enough Harmony for traveling.");
+            args.IsEnabled = false;
+            return true;
+        }
+
+    }
+    
+
+    private void RootTravelConsequence(Vec2 location)
+    {
+        LeaveSettlementAction.ApplyForParty(MobileParty.MainParty);
+        GameMenu.ExitToLast();
+        Campaign.Current.MainParty.Position2D =MobilePartyHelper.FindReachablePointAroundPosition(location,0f);
+
+        if (Hero.MainHero.HasAttribute("WETravelHealUpgrade"))
+        {
+            var heroes = MobileParty.MainParty.GetMemberHeroes();
+            foreach (var hero in heroes)
+            {
+                hero.Heal(1000,false);
+            }
+
+            var list = MobileParty.MainParty.MemberRoster.GetTroopRoster();
+            for (var index = 0; index < list.Count; index++)
+            {
+                var element = list[index];
+                var woundedNumber = element.WoundedNumber;
+
+                MobileParty.MainParty.MemberRoster.AddToCountsAtIndex(index, 0, -woundedNumber);
+            }
+        }
+
+    }
+    
+    private void RootUpgradeConsequence(int upgradeCost, string worldRootTargetLocation)
+    {
+        Hero.MainHero.AddAttribute(worldRootTargetLocation);
+        Hero.MainHero.AddCultureSpecificCustomResource(-upgradeCost);
+        GameMenu.SwitchToMenu("oak_of_ages_roots_menu");
+    }
+
 
 
     private void AddBranchesOfTheOakMenu(CampaignGameStarter starter)
     {
-        starter.AddGameMenu("oak_of_ages_branches_menu", "Branches of The Oak", OakOfAgeMenuInit);
+        starter.AddGameMenu("oak_of_ages_branches_menu", "Branches of The Oak", null);
         
         starter.AddGameMenuOption("oak_of_ages_branches_menu", "branchMenu_A", "Increase party Size. {PARTYSIZEUPGRADECOST}{FORESTHARMONY}",args => 
             BranchUpgradeCondition(args,PartySizeUpgradeCost, "WEPartySizeUpgrade"),_ => UpgradeConsequence(PartySizeUpgradeCost,"WEPartySizeUpgrade" ));
@@ -123,7 +258,7 @@ public class OakOfAgesMenuLogic : TORBaseSettlementMenuLogic
         starter.AddGameMenuOption("oak_of_ages_branches_menu", "branchMenu_C", "Increase the daily harmony gain. {GAINUPGRADECOST}{FORESTHARMONY}",
             args => BranchUpgradeCondition(args,GainUpgradeCost,"WEGainUpgrade"),_ => UpgradeConsequence(GainUpgradeCost, "WEGainUpgrade"));
         
-        starter.AddGameMenuOption("oak_of_ages_branches_menu", "branchMenu_D", " Troop {FORESTHARMONY1} Upkeep  reduction. {UPKEEPUPGRADECOST}{FORESTHARMONY}", 
+        starter.AddGameMenuOption("oak_of_ages_branches_menu", "branchMenu_D", " Troop harmony upkeep  reduction. {UPKEEPUPGRADECOST}{FORESTHARMONY}", 
             args => BranchUpgradeCondition(args,TroopUpkeepUpgradeCost, "WEUpkeepUpgrade") ,_ => UpgradeConsequence(TroopUpkeepUpgradeCost, "WEGainUpgrade"));
         
         starter.AddGameMenuOption("oak_of_ages_branches_menu", "branchMenu_leave", "Leave...",
