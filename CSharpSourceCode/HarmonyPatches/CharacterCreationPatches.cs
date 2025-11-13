6a015139ea4db91a42e97a2a323dd964e65ae41f
Linus Tiemann <linus-tiemann@live.de>
2025-11-13 01:14:15 +0100

    adds positive effect patch
    the patch allows to change the Positive Effects in the Character Creation. previously this was not a thing now it tells you "you have 0 unspend points" - which is amzingly informing. this got replaced with empty, and would otherwise use our optional positive effects from the cc options
    
    also remove unnecessary patch

--- CSharpSourceCode/HarmonyPatches/CharacterCreationPatches.cs
+++ CSharpSourceCode/HarmonyPatches/CharacterCreationPatches.cs
@@ -1,7 +1,10 @@
-﻿using HarmonyLib;
+﻿using System.Collections.Generic;
+using HarmonyLib;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CharacterCreationContent;
 using TaleWorlds.CampaignSystem.ViewModelCollection.CharacterCreation;
 using TaleWorlds.Core;
+using TaleWorlds.Localization;
 using TOR_Core.Utilities;
 
 namespace TOR_Core.HarmonyPatches
@@ -9,6 +12,9 @@ namespace TOR_Core.HarmonyPatches
     [HarmonyPatch]
     public static class CharacterCreationPatches
     {
+        // Maintain order of races added to the faction list.
+        internal static readonly Dictionary<NarrativeMenuOptionArgs, TextObject> CustomPositiveEffects = new Dictionary<NarrativeMenuOptionArgs, TextObject>();
+
         [HarmonyPrefix]
         [HarmonyPatch(typeof(CharacterCreationCultureStageVM), "SortCultureList")]
         public static bool DoNotSortCultures()
@@ -16,51 +22,23 @@ namespace TOR_Core.HarmonyPatches
             return false;
         }
 
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(CharacterCreationCultureStageVM), "InitializePlayersFaceKeyAccordingToCultureSelection")]
-        public static bool UpdateRaceAndFaceKey(CharacterCreationCultureVM selectedCulture)
+        // Patch to override positive effect text for character creation options
+        [HarmonyPatch(typeof(NarrativeMenuOptionArgs), "PositiveEffectText", MethodType.Getter)]
+        public class PositiveEffectTextPatch
         {
-            //TODO: replace these, they are copied from vanilla
-            string default_elf = "<BodyProperties version='4' age='25.84' weight='0.5000' build='0.5000'  key='000BAC088000100DB976648E6774B835537D86629511323BDCB177278A84F667017776140748B49500000000000000000000000000000000000000003EFC5002'/>";
-            string default_empire = "<BodyProperties version='4' age='25.84' weight='0.5000' build='0.5000'  key='000500000000000D797664884754DCBAA35E866295A0967774414A498C8336860F7776F20BA7B7A500000000000000000000000000000000000000003CFC2002'/>";
-            string default_bretonnia = "<BodyProperties version='4' age='25.84' weight='0.5000' build='0.5000'  key='001CB80CC000300D7C7664876753888A7577866254C69643C4B647398C95A0370077760307A7497300000000000000000000000000000000000000003AF47002'/>";
-            string default_vc = "<BodyProperties version='4' age='25.84' weight='0.5000' build='0.5000'  key='0028C80FC000100DBA756445533377873CD1833B3101B44A21C3C5347CA32C260F7776F20BBC35E8000000000000000000000000000000000000000042F41002'/>";
-            string keyValue;
+            static bool Prefix(NarrativeMenuOptionArgs __instance, ref TextObject __result)
+            {
+                // Check if this args instance has custom positive effect text
+                if (CustomPositiveEffects.TryGetValue(__instance, out TextObject customText))
+                {
+                    __result = customText;
+                    return false; // Skip original method
+                }
 
-            if (selectedCulture.Culture.StringId == TORConstants.Cultures.ASRAI || selectedCulture.Culture.StringId == TORConstants.Cultures.EONIR)
-            {
-                keyValue = default_elf;
-                CharacterObject.PlayerCharacter.Race = FaceGen.GetRaceOrDefault("elf");
+                // No custom text - return empty instead of showing "0 unspent" text
+                __result = TextObject.GetEmpty();
+                return false; // Skip original method
             }
-            else if (selectedCulture.Culture.StringId == TORConstants.Cultures.EMPIRE)
-            {
-                keyValue = default_empire;
-                CharacterObject.PlayerCharacter.Race = FaceGen.GetRaceOrDefault("human");
-            }
-            else if (selectedCulture.Culture.StringId == TORConstants.Cultures.BRETONNIA || selectedCulture.Culture.StringId == TORConstants.Cultures.MOUSILLON)
-            {
-                keyValue = default_bretonnia;
-                CharacterObject.PlayerCharacter.Race = FaceGen.GetRaceOrDefault("human");
-            }
-            else if (selectedCulture.Culture.StringId == TORConstants.Cultures.SYLVANIA)
-            {
-                keyValue = default_vc;
-                CharacterObject.PlayerCharacter.Race = FaceGen.GetRaceOrDefault("human");
-            }
-            else
-            {
-                keyValue = default_empire;
-                CharacterObject.PlayerCharacter.Race = FaceGen.GetRaceOrDefault("human");
-            }
-
-            CharacterObject.PlayerCharacter.HeroObject.Culture = selectedCulture.Culture;
-
-            if (BodyProperties.FromString(keyValue, out BodyProperties properties))
-            {
-                CharacterObject.PlayerCharacter.UpdatePlayerCharacterBodyProperties(properties, CharacterObject.PlayerCharacter.Race, CharacterObject.PlayerCharacter.IsFemale);
-            }
-
-            return false;
         }
     }
 }
