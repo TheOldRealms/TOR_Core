23788b68adf76e85dee0efdfa95a3ee5b284795b
SlyDevil <dkmac.15@gmail.com>
2025-11-21 13:33:56 -0500

    Dismemberment, cursed site, skills, and text fixes. Native companion behavior replaced.
    Fixed dismemberment crash in friendly missions.
    Companion behavior is now active, but there are some minor fixes that are not yet implemented.
    Fixed crash when hovering mouse over a cursed site. The decal has been made more visible to help with uneven terrain near the settlement.
    Fixed skills granting too large of bonuses due to them not being adjusted as percentages by the game anymore.
    Added a text line to the shader caching menu option describing how a user knows when it's complete.
    Added a late patch and removed SubModule initialization of GameText manager.
    Added a xslt to delete all of the native wanderers.
    Fixed town fortification levels being at 2 instead of 3 for 3/4 of the Towns - this is purely for settlement construction purposes; due to how scene levels are loaded, the intended scene was always loaded.
    
    Added some placeholder text lines for trainer npcs.

--- CSharpSourceCode/CampaignMechanics/TORCustomSettlement/TORBaseSettlementComponent.cs
+++ CSharpSourceCode/CampaignMechanics/TORCustomSettlement/TORBaseSettlementComponent.cs
@@ -56,7 +56,7 @@ namespace TOR_Core.CampaignMechanics.TORCustomSettlement
     public abstract class BaseRaiderSpawnerComponent : TORBaseSettlementComponent
     {
         public virtual int BattlePartySize => 250;
-        public int RaidingPartyCount => MobileParty.All.Where(x => x.IsRaidingParty() && x.HomeSettlement == Settlement).Count();
+        public int RaidingPartyCount => MobileParty.All.Where(x => x.IsRaidingParty() && x.HomeSettlement == Settlement).Count(); //Sly : that's a lot of parties being checked
         public abstract string BattleSceneName { get; }
         public bool IsBattleUnderway { get; set; }
         public abstract List<string> RewardItemIds { get; }
--- CSharpSourceCode/Utilities/TORGameStarterHelper.cs
+++ CSharpSourceCode/Utilities/TORGameStarterHelper.cs
@@ -21,7 +21,7 @@ namespace TOR_Core.Utilities
             starter.RemoveBehaviors<KingdomDecisionProposalBehavior>();
             starter.RemoveBehaviors<RebellionsCampaignBehavior>();
             starter.RemoveBehaviors<SallyOutsCampaignBehavior>();
-            //starter.RemoveBehaviors<CompanionsCampaignBehavior>();
+            starter.RemoveBehaviors<CompanionsCampaignBehavior>();
 
             var issues = starter.CampaignBehaviors.Where(x => x.GetType().FullName.Contains("Issue")).ToList();
             foreach (var issue in issues)
