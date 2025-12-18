17d552cbd0b97ca3c5a19b73d08ee7f02178fe4f
Linus Tiemann <linus-tiemann@live.de>
2025-12-18 22:40:15 +0100

    updated ability workflow

---
+++ CSharpSourceCode/AbilitySystem/Scripts/KnightlyStrikeScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.LinQuick;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.TwoDimension;
+using TOR_Core.CharacterDevelopment.CareerSystem;
+using TOR_Core.Extensions;
+using TOR_Core.Items;
+
+namespace TOR_Core.AbilitySystem.Scripts
+{
+    public class KnightlyStrike : CareerAbilityScript
+    {
+        protected override void OnAfterTick(float dt)
+        {
+            base.OnAfterTick(dt);
+            
+            var duration = this.Ability.Template.Duration; // is this the modified Duration?
+
+            float additionalLoads = Hero.MainHero.GetAllCareerChoices().WhereQ(x => x.Contains("Keystone")).Count();
+
+            additionalLoads +=  this.Ability.Template.ScaleVariable1;
+            
+            int loads = (int)Mathf.Clamp(additionalLoads, 1, 15);
+            
+
+            var traitList = new List<ItemTrait>();
+            
+            
+
+            if (Hero.MainHero.HasCareerChoice("PathOfGloryKeystone"))
+            {
+                var holyTrait = CareerHelper.GetTraitForReligion(Hero.MainHero, Hero.MainHero.GetDominantReligion());
+                if (holyTrait != null && holyTrait != ItemTrait.Invalid) traitList.Add(holyTrait);
+            }
+
+            var knightlytrait = ItemTrait.All.FirstOrDefaultQ(x => x.ItemTraitStringId == "ca_knightlystrike");
+            
+            
+            
+            
+            if (knightlytrait != null) traitList.Add(knightlytrait);
+
+            CasterAgent.ApplyStatusEffect("knightly_strike", CasterAgent, duration, false, false, true);
+            for (int i = 0; i < loads; i++)
+            {
+                CasterAgent.ApplyStatusEffect("knightly_strike", CasterAgent, duration, false, false, true);
+            }
+            
+            
+        }
+    }
+}
--- CSharpSourceCode/Items/WeaponHitScripts/KnightlyStrikeHitScript.cs
+++ CSharpSourceCode/Items/WeaponHitScripts/KnightlyStrikeHitScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Schema;
 using TaleWorlds.CampaignSystem;
@@ -27,11 +27,6 @@ public class KnightlyStrikeHitScript : BaseWeaponHitScript
             statusEffectComponent.RemoveStatusEffect("knightly_strike");
             var list = statusEffectComponent.GetTemporaryAttributes(true).Where(x => x == "KnightlyStrike").ToList();
             knightlyStrikes.AddRange(list);
-        }
-
-        if (Hero.MainHero.HasCareerChoice("WrathAgainstChaosKeystone"))
-        {
-            attackingAgent.ApplyStatusEffect("knightly_strike_ws", attackingAgent, 5, true, false, false);
         }
 
         if (knightlyStrikes.Count > 0)
