0533746d4a8011d0a5ea9caa9408b0a48d42989f
Linus Tiemann <linus-tiemann@live.de>
2025-12-30 15:05:50 +0100

    removed warrior priest legacy perk

--- CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/WarriorPriestUlricCareerChoices.cs
+++ CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/WarriorPriestUlricCareerChoices.cs
@@ -227,7 +227,7 @@ namespace TOR_Core.CharacterDevelopment.CareerSystem.Choices
             _teachingsOfTheWinterFatherPassive2.Initialize(CareerID, "Praying at a shrine of Ulric refills player health completely (5 day cooldown).", "TeachingsOfTheWinterFather", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect());
             _teachingsOfTheWinterFatherPassive3.Initialize(CareerID, "Attacks deal bonus damage against shields.", "TeachingsOfTheWinterFather", false, ChoiceType.Passive, null);
             _teachingsOfTheWinterFatherPassive4.Initialize(CareerID, "Increases range damage resistance of melee troops by 20%.", "TeachingsOfTheWinterFather", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(PassiveEffectType.TroopResistance, new DamageProportionTuple(DamageType.Physical, 20), AttackTypeMask.Ranged,
-                (attacker, victim, mask) => victim.BelongsToMainParty() && !(victim.IsMainAgent || victim.IsHero) && mask == AttackTypeMask.Melee));
+                (attacker, victim, mask) => victim.BelongsToMainParty() && !(victim.IsMainAgent || victim.IsHero) && mask == AttackTypeMask.Ranged));
 
             _frostsBitePassive1.Initialize(CareerID, "Extra frost damage (10%).", "FrostsBite", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(PassiveEffectType.Damage, new DamageProportionTuple(DamageType.Frost, 10), AttackTypeMask.Melee));
             _frostsBitePassive2.Initialize(CareerID, "Add 10% frost damage to melee troops.", "FrostsBite", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(PassiveEffectType.TroopDamage, new DamageProportionTuple(DamageType.Frost, 10), AttackTypeMask.Melee));
--- CSharpSourceCode/Models/TORMobilePartyFoodConsumptionModel.cs
+++ CSharpSourceCode/Models/TORMobilePartyFoodConsumptionModel.cs
@@ -97,51 +97,6 @@ namespace TOR_Core.Models
 
         private void AddCareerSpecificFoodPerks(ref ExplainedNumber values, MobileParty party)
         {
-            var choices = party.LeaderHero.GetAllCareerChoices();
-
-            if (choices.Contains("SigmarsProclaimerPassive3"))
-            {
-                bool includeRegularTroops = choices.Contains("ArchLectorPassive2");
-                var choice = TORCareerChoices.GetChoice("SigmarsProclaimerPassive3");
-                var perkValue = AddSigmarsProclaimerPerk(values, party, choice, includeRegularTroops);
-                values.Add(perkValue, choice.BelongsToGroup.Name);
-            }
-
-
-        }
-
-        private float AddSigmarsProclaimerPerk(ExplainedNumber values, MobileParty party, CareerChoiceObject perkChoice, bool includeRegularTroops)
-        {
-            if (perkChoice == null) return 0;
-            var choices = party.LeaderHero.GetAllCareerChoices();
-
-
-            var troops = party.MemberRoster.GetTroopRoster();
-            var sigmarRiteTroops = new MBList<TroopRosterElement>();
-
-            foreach (var troopRosterElement in troops.Where(troopRosterElement => troopRosterElement.Character.IsSoldier)) //could be all a nice query , doesn't work for whatever reason
-            {
-                if (!troopRosterElement.Character.UnitBelongsToCult("cult_of_sigmar"))
-                {
-                    if (troopRosterElement.Character.IsReligiousUnit())
-                        continue;
-                    if (includeRegularTroops)
-                    {
-                        sigmarRiteTroops.Add(troopRosterElement);
-                    }
-                }
-                else
-                {
-                    sigmarRiteTroops.Add(troopRosterElement);
-                }
-            }
-
-            var count = sigmarRiteTroops.Sum(x => x.Number);
-            if (perkChoice.Passive == null) return 0f;
-            var effectMagnitude = perkChoice.Passive.EffectMagnitude;
-            if (perkChoice.Passive.InterpretAsPercentage) effectMagnitude /= 100;
-            float basefoodConsumptionForRoster = ((float)count / NumberOfMenOnMapToEatOneFood);
-            return basefoodConsumptionForRoster * effectMagnitude;
         }
 
 
