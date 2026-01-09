d9f42437dcbd96fe22ef07db4e40056dfc959449
Linus Tiemann <43908801+Z3rca@users.noreply.github.com>
2026-01-10 00:52:48 +0100

    fix mousillon necromancer spawn location. fix tor strings for activities. add helper texts for code to match strings easier
    added todos for missing activities

--- CSharpSourceCode/CampaignMechanics/ServeAsAHireling/ServeAsAHirelingActivities.cs
+++ CSharpSourceCode/CampaignMechanics/ServeAsAHireling/ServeAsAHirelingActivities.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using TaleWorlds.Core;
 using TOR_Core.CharacterDevelopment;
@@ -16,210 +16,209 @@ public class ServeAsAHirelingActivities
             //Mousillon
             {
                 TORCareers.BlackGrailKnight, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.Charm,
-                    DefaultSkills.Riding,
-                    DefaultSkills.Polearm,
-                    DefaultSkills.Roguery
+                    DefaultSkills.OneHanded,  // Practice sword-fighting
+                    DefaultSkills.Charm,      // Spread The Lady's Lie among the nobles
+                    DefaultSkills.Riding,     // Ride ahead of the army
+                    DefaultSkills.Polearm,    // Saddle up and joust with other nobles
+                    DefaultSkills.Roguery     // Discipline the peasantry
                 ]
             },
             //Bretonnia
             {
                 TORCareers.GrailKnight, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.Charm,
-                    DefaultSkills.Riding,
-                    DefaultSkills.Polearm,
-                    TORSkills.Faith
+                    DefaultSkills.OneHanded,  // Practice sword-fighting
+                    DefaultSkills.Charm,      // Socialize with other nobles
+                    DefaultSkills.Riding,     // Ride ahead of the army
+                    DefaultSkills.Polearm,    // Practice jousting
+                    TORSkills.Faith           // Pray to the Lady of the Lake
                 ]
             },
             {
                 TORCareers.GrailDamsel, [
-                    DefaultSkills.Riding,
-                    TORSkills.Spellcraft,
-                    TORSkills.Faith,
-                    DefaultSkills.Steward,
-                    DefaultSkills.Medicine
+                    DefaultSkills.Riding,     // Converse with the stabled horses
+                    TORSkills.Spellcraft,     // Prepare warding rituals
+                    TORSkills.Faith,          // Bless the troops in the name of The Lady
+                    DefaultSkills.Steward,    // Advise the army commander
+                    DefaultSkills.Medicine    // Mediate the quarreling nobles
                 ]
             },
             //Vampire Counts
             {
                 TORCareers.Necromancer, [
-                    DefaultSkills.Riding,
-                    TORSkills.Spellcraft,
-                    TORSkills.Faith,
-                    DefaultSkills.Steward,
-                    DefaultSkills.Medicine
+                    TORSkills.Spellcraft,     // Study tomes of knowledge
+                    DefaultSkills.Roguery,    // Raid nearby cemeteries
+                    DefaultSkills.Medicine,   // Experiment on dead bodies
+                    DefaultSkills.Leadership, // Send your minions ahead of the army
+                    DefaultSkills.Steward     // Assist the vampire lieutenants
                 ]
             },
             {
                 TORCareers.BloodKnight, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Riding,
-                    DefaultSkills.Tactics,
-                    DefaultSkills.Leadership
+                    DefaultSkills.OneHanded,  // Train with a sword
+                    DefaultSkills.TwoHanded,  // Train with a great sword
+                    DefaultSkills.Riding,     // Ride in the vanguard
+                    DefaultSkills.Tactics,    // Participate in strategy meetings
+                    DefaultSkills.Leadership  // Separate the weak from the strong
                 ]
             },
             {
                 TORCareers.MinorVampire, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.Charm,
-                    TORSkills.Spellcraft,
-                    DefaultSkills.Roguery,
-                    DefaultSkills.Leadership,
+                    DefaultSkills.OneHanded,  // Spar with the troops
+                    DefaultSkills.Charm,      // Curry favor with the commander
+                    TORSkills.Spellcraft,     // Focus on the vile energies in the area
+                    DefaultSkills.Roguery,    // Search for prey to feed on
+                    DefaultSkills.Leadership  // Command the sheep
                 ]
             },
             {
                 TORCareers.Necrarch, [
-                    TORSkills.Spellcraft,
-                    DefaultSkills.Roguery,
-                    DefaultSkills.Medicine,
-                    DefaultSkills.Engineering,
-                    DefaultSkills.Steward
+                    TORSkills.Spellcraft,     // Hone your Witchsight
+                    DefaultSkills.Roguery,    // Search for prey to feed on
+                    DefaultSkills.Medicine,   // Dissect test subjects
+                    DefaultSkills.Engineering,// Study arcane blueprints
+                    DefaultSkills.Steward     // Issue orders to your servants
                 ]
             },
             //Empire
             {
                 TORCareers.Mercenary, [
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Bow,
-                    TORSkills.GunPowder,
-                    DefaultSkills.Trade,
-                    DefaultSkills.Tactics,
+                    DefaultSkills.TwoHanded,  // Discuss techniques with shock troops
+                    DefaultSkills.Bow,        // Target practice with archer regiments
+                    TORSkills.GunPowder,      // Maintain gunpowder weapons
+                    DefaultSkills.Trade,      // Barter and haggle with the quartermaster
+                    DefaultSkills.Tactics     // Eavesdrop by the commander's tent
                 ]
             },
             {
                 TORCareers.ImperialMagister, [
-                    TORSkills.Spellcraft,
-                    DefaultSkills.Steward,
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.Medicine,
-                    DefaultSkills.Tactics
+                    TORSkills.Spellcraft,     // Focus on your studies
+                    DefaultSkills.Steward,    // Assist your lord with accounting tasks
+                    DefaultSkills.OneHanded,  // Join the sparring soldiers
+                    DefaultSkills.Medicine,   // Care for the wounded troops
+                    DefaultSkills.Tactics     // Advise the commander
                 ]
             },
             {
                 TORCareers.WitchHunter, [
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Crossbow,
-                    TORSkills.GunPowder,
-                    TORSkills.Faith,
-                    DefaultSkills.OneHanded
+                    DefaultSkills.TwoHanded,  // Great sword sparring
+                    DefaultSkills.Crossbow,   // Target practice with crossbows
+                    TORSkills.GunPowder,      // Maintain gunpowder weapons
+                    TORSkills.Faith,          // Lecture on the dangers of Corruption
+                    DefaultSkills.OneHanded   // Practice with a rapier
                 ]
             },
             {
                 TORCareers.WarriorPriest, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Athletics,
-                    DefaultSkills.Medicine,
-                    TORSkills.Faith,
+                    DefaultSkills.OneHanded,  // Spar with the troops
+                    DefaultSkills.TwoHanded,  // Train with a warhammer
+                    DefaultSkills.Athletics,  // Help set up tents
+                    DefaultSkills.Medicine,   // Assist the priestesses of Shallya
+                    TORSkills.Faith           // Preach about Sigmar Heldenhammer
                 ]
             },
             {
                 TORCareers.WarriorPriestUlric, [
-                    DefaultSkills.Scouting,
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Athletics,
-                    DefaultSkills.Leadership,
-                    TORSkills.Faith,
+                    DefaultSkills.Scouting,   // Scour the wilds
+                    DefaultSkills.TwoHanded,  // Train with a battleaxe
+                    DefaultSkills.Athletics,  // Help with firewood
+                    DefaultSkills.Leadership, // Inspire the troops to drive away weakness
+                    TORSkills.Faith           // Preach the teachings of Ulric
                 ]
             },
             {
                 TORCareers.KnightOldWorld, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Polearm,
-                    DefaultSkills.Riding,
-                    DefaultSkills.Leadership,
+                    DefaultSkills.OneHanded,  // Sparring practice with a sword
+                    DefaultSkills.TwoHanded,  // Sparring practice with a greatsword
+                    DefaultSkills.Polearm,    // Joust with other cavalrymen
+                    DefaultSkills.Riding,     // Tend to your mount
+                    DefaultSkills.Leadership  // Give battlefield advice to common troops
                 ]
             },
             //Woodelves
             {
                 TORCareers.Waywatcher, [
-                    DefaultSkills.Bow,
-                    DefaultSkills.Scouting,
-                    DefaultSkills.Roguery,
-                    DefaultSkills.Athletics,
-                    DefaultSkills.Medicine
+                    DefaultSkills.Bow,        // Target practice in solitude
+                    DefaultSkills.Scouting,   // Climb trees and scout the area
+                    DefaultSkills.Roguery,    // Cover the perimeter and set up traps
+                    DefaultSkills.Athletics,  // Advanced sparring sessions
+                    DefaultSkills.Medicine    // Gather medical herbs
                 ]
             },
             {
                 TORCareers.Spellsinger, [
-                    TORSkills.Spellcraft,
-                    DefaultSkills.Riding,
-                    TORSkills.Faith,
-                    DefaultSkills.Charm,
-                    DefaultSkills.Medicine
+                    TORSkills.Spellcraft,     // Commune with forest spirits
+                    DefaultSkills.Riding,     // Converse with the stabled horses
+                    TORSkills.Faith,          // Offer a prayer to Isha, the Mother
+                    DefaultSkills.Charm,      // Discuss diplomatic matters with the commander
+                    DefaultSkills.Medicine    // Gather medicinal herbs
                 ]
             },
             {
                 TORCareers.Warden, [
-                    DefaultSkills.Polearm,
-                    DefaultSkills.Throwing,
-                    DefaultSkills.Bow,
-                    DefaultSkills.Scouting,
-                    DefaultSkills.Leadership,
+                    DefaultSkills.Polearm,    // Sparring practice with spears
+                    DefaultSkills.Throwing,   // Javelin target practice
+                    DefaultSkills.Bow,        // Bow target practice
+                    DefaultSkills.Scouting,   // Scout the perimeter with your hawk
+                    DefaultSkills.Leadership  // Discuss army movements with other nobles
                 ]
             },
             //Eonir
             {
                 TORCareers.GreyLord, [
-                    TORSkills.Spellcraft,
-                    DefaultSkills.Steward,
-                    DefaultSkills.Leadership,
-                    DefaultSkills.Charm,
-                    DefaultSkills.Medicine
+                    TORSkills.Spellcraft,     // Study manuscripts
+                    DefaultSkills.Steward,    // Engage in book keeping tasks
+                    DefaultSkills.Leadership, // Hold speeches about the dragons of Caledor
+                    DefaultSkills.Charm,      // Maintain Asurian etiquette
+                    DefaultSkills.Medicine    // Experiment with alchemistic substances
                 ]
             },
-            
+
             //Dwarfs
-            
             {
                 TORCareers.Ironbreaker, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.Crafting,
-                    DefaultSkills.Scouting,
-                    DefaultSkills.Athletics,
-                    TORSkills.GunPowder,
+                    DefaultSkills.OneHanded,  // Train with an az
+                    DefaultSkills.Crafting,   // Maintain your armor and weapons
+                    DefaultSkills.Scouting,   // Scout nearby tunnels
+                    DefaultSkills.Athletics,  // Arm wrestle with other soldiers
+                    TORSkills.GunPowder       // Help the army engineer with grenades
                 ]
             },
             {
                 TORCareers.Slayer, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Athletics,
-                    TORSkills.Faith,
-                    DefaultSkills.Medicine,
+                    DefaultSkills.OneHanded,  // Train with an az
+                    DefaultSkills.TwoHanded,  // Practice with a great az
+                    DefaultSkills.Athletics,  // Patrol the camp perimeter
+                    TORSkills.Faith,          // Repeat the words of your Slayer Oath
+                    DefaultSkills.Medicine    // Drown your shame in ale
                 ]
             },
             {
                 TORCareers.Runelord, [
-                    TORSkills.Spellcraft,
-                    TORSkills.Faith,
-                    DefaultSkills.Crafting,
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.Trade,
+                    DefaultSkills.TwoHanded,     // TODO: Add activity string
+                    TORSkills.Faith,          // TODO: Add activity string
+                    DefaultSkills.Crafting,   // TODO: Add activity string
+                    DefaultSkills.OneHanded,  // TODO: Add activity string
+                    DefaultSkills.Trade       // TODO: Add activity string
                 ]
             },
-            
+
             //Orcs
             {
                 TORCareers.OrcBoss, [
-                    DefaultSkills.OneHanded,
-                    DefaultSkills.TwoHanded,
-                    DefaultSkills.Polearm,
-                    DefaultSkills.Athletics,
-                    DefaultSkills.Leadership,
+                    DefaultSkills.OneHanded,  // Smash fings wiv a smaller choppa
+                    DefaultSkills.TwoHanded,  // Smash fings wiv da biggest choppa
+                    DefaultSkills.Polearm,    // Smash fings wiv a long choppa
+                    DefaultSkills.Athletics,  // Yell rude names at da Arrer Boys
+                    DefaultSkills.Leadership  // Explain Gork an' Mork to sum Trolls
                 ]
             },
             {
                 TORCareers.OrcShaman, [
-                    TORSkills.Spellcraft,
-                    TORSkills.Faith,
-                    DefaultSkills.Medicine,
-                    DefaultSkills.Leadership,
-                    DefaultSkills.Roguery,
+                    TORSkills.Spellcraft,     // TODO: Add activity string
+                    TORSkills.Faith,          // TODO: Add activity string
+                    DefaultSkills.Medicine,   // TODO: Add activity string
+                    DefaultSkills.Leadership, // TODO: Add activity string
+                    DefaultSkills.Roguery     // TODO: Add activity string
                 ]
             },
 
