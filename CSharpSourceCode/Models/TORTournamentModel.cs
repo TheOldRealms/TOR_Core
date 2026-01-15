c7dd0337c1b32af9449277b893538b77e843f503
SlyDevil <dkmac.15@gmail.com>
2026-01-15 16:49:14 -0500

    Raiding parties, quest parties, and chaos cultured parties no longer consume food so they don't risk starving. Raiding parties now move at normal speeds due to not taking overmanned penalties. Party weights for troops calculated later to prevent their subtractions from making other bonuses have variable outputs depending on troop counts. Tournament rewards now draw from the current culture of the town they take place in : normal rewards are t4- and elite rewards are t5+. Fixed map indices with a large number of available scenes writing a list outside the console space. Reduced the number of bandits that can spawn. Added a missing greenskin game host as well as some skillsets.

--- CSharpSourceCode/Models/TORTournamentModel.cs
+++ CSharpSourceCode/Models/TORTournamentModel.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.GameComponents;
 using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.CampaignSystem.TournamentGames;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.LinQuick;
+using TaleWorlds.ObjectSystem;
 using TOR_Core.BattleMechanics.CustomArenaModes;
 using TOR_Core.CampaignMechanics.Assimilation;
 using TOR_Core.Utilities;
@@ -35,6 +40,56 @@ namespace TOR_Core.Models
         {
             //return 1f; //DEBUG
             return base.GetTournamentStartChance(town);
+        }
+
+        public override MBList<ItemObject> GetRegularRewardItems(Town town, int regularRewardMinValue, int regularRewardMaxValue)
+        {
+            //Sly : if the culture has no items, that should be looked into. Not a fan of building a 2nd list to store every other culture's items.
+            MBList<ItemObject> mBList = new MBList<ItemObject>();
+            MBList<ItemObject> mBList2 = new MBList<ItemObject>();
+            foreach (ItemObject item in Game.Current.ObjectManager.GetObjectTypeList<ItemObject>())
+            {
+                if (!item.NotMerchandise && item.Tier < ItemObject.ItemTiers.Tier5 && (item.IsCraftedWeapon || item.IsMountable || item.ArmorComponent != null) && !item.IsCraftedByPlayer)
+                {
+                    if (item.Culture == town.Culture)
+                    {
+                        mBList.Add(item);
+                    }
+                    else
+                    {
+                        mBList2.Add(item);
+                    }
+                }
+            }
+
+            //Sly : banners left out because native will collect all banners even unrelated cultures
+
+            if (mBList.IsEmpty())
+            {
+                return mBList2;
+            }
+
+            return mBList;
+        }
+
+        public override MBList<ItemObject> GetEliteRewardItems(Town town, int regularRewardMinValue, int regularRewardMaxValue)
+        {
+            MBList<ItemObject> mBList = new MBList<ItemObject>();
+            foreach (ItemObject item in Game.Current.ObjectManager.GetObjectTypeList<ItemObject>())
+            {
+                if (!item.NotMerchandise && item.Culture == town.Culture && item.Tier > ItemObject.ItemTiers.Tier4 && (item.IsCraftedWeapon || item.IsMountable || item.ArmorComponent != null) && !item.IsCraftedByPlayer)
+                {
+                    mBList.Add(item);
+                }
+            }
+
+            if (mBList.IsEmpty())
+            {
+                TORCommon.Log("TORTournamentModel : no t5 or 6 item found belonging to the " + town.Culture.StringId + " culture. They get the first item object instead.", NLog.LogLevel.Warn);
+                mBList.Add(Game.Current.ObjectManager.GetFirstObject<ItemObject>());
+            }
+
+            return mBList;
         }
     }
 }
