1263df9991d274bbe7cfa5e646042d07c61910b1
Linus Tiemann <linus-tiemann@live.de>
2026-03-22 03:32:07 +0100

    fix blasting charge explosion for good

--- CSharpSourceCode/BattleMechanics/Firearms/FirearmsMissionLogic.cs
+++ CSharpSourceCode/BattleMechanics/Firearms/FirearmsMissionLogic.cs
@@ -251,15 +251,12 @@ namespace TOR_Core.BattleMechanics.Firearms
             }
 
             // play sound of shot and create shot effects
+            // Hand-thrown grenades and blasting charges don't make firing sounds, only explosion on impact
             if (!shooterAgent.WieldedWeapon.AmmoWeapon.Item.StringId.Contains("grenade") && !shooterAgent.WieldedWeapon.AmmoWeapon.Item.StringId.Contains("blasting_charges"))
             {
                 // run particles of smoke
                 Mission.AddParticleSystemBurstByName("handgun_shoot_2", frame, false);
                 CreateMuzzleFireSound(position);
-            }
-            else
-            {
-                CreateMuzzleFireSound(position, MuzzleFireSoundType.Grenadelauncher);
             }
         }
 
@@ -393,7 +390,7 @@ namespace TOR_Core.BattleMechanics.Firearms
 
             var pos = missileObj.Entity.GlobalPosition;
 
-            if (missileObj.Weapon.Item.StringId.Contains("grenade"))
+            if (missileObj.Weapon.Item.StringId.Contains("grenade") || missileObj.Weapon.Item.StringId.Contains("blasting_charges"))
             {
                 RunExplosionSoundEffects(pos, "mortar_explosion_1");
                 RunExplosionVisualEffects(pos, "cannonball_explosion_8");
