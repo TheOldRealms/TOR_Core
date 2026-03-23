54ceba209586cd5945a8ca868160fa28b237b3f5
Linus Tiemann <linus-tiemann@live.de>
2026-03-23 19:17:44 +0100

    high and dark weaver limitation (Spellsinger)

--- CSharpSourceCode/AbilitySystem/Spells/LoreObject.cs
+++ CSharpSourceCode/AbilitySystem/Spells/LoreObject.cs
@@ -39,7 +39,11 @@ namespace TOR_Core.AbilitySystem.Spells
                 _lores.Add("LoreOfLife", new LoreObject("LoreOfLife", "Lore of Life", "lifemagic_symbol", new List<string>() { TORConstants.Cultures.SYLVANIA, TORConstants.Cultures.MOUSILLON, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }));
                 _lores.Add("LoreOfMetal", new LoreObject("LoreOfMetal", "Lore of Metal", "metalmagic_symbol", new List<string>() { TORConstants.Cultures.SYLVANIA, TORConstants.Cultures.MOUSILLON, TORConstants.Cultures.BRETONNIA, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }));
                 _lores.Add("LoreOfBeasts", new LoreObject("LoreOfBeasts", "Lore of Beasts", "beastmagic_symbol", new List<string>() { TORConstants.Cultures.SYLVANIA, TORConstants.Cultures.MOUSILLON, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }));
-                _lores.Add("HighMagic", new LoreObject("HighMagic", "High Magic", "highmagic_symbol", new List<string>() { TORConstants.Cultures.EMPIRE, TORConstants.Cultures.BRETONNIA, TORConstants.Cultures.MOUSILLON, TORConstants.Cultures.SYLVANIA, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }, true));
+
+                // NOTE: High Magic and Dark Magic are disabled for ASRAI (Wood Elves) in general, BUT Spellsingers can still access them
+                // through the special Spellweaver dialog (SpellTrainerInTownBehavior.cs:390-403) which bypasses culture restrictions
+                // by offering a hardcoded list of both lores. This ensures only Spellsingers can become High/Dark Weavers, not all Wood Elf casters.
+                _lores.Add("HighMagic", new LoreObject("HighMagic", "High Magic", "highmagic_symbol", new List<string>() { TORConstants.Cultures.EMPIRE, TORConstants.Cultures.BRETONNIA, TORConstants.Cultures.MOUSILLON, TORConstants.Cultures.SYLVANIA, TORConstants.Cultures.ASRAI, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }, true));
 
                 _lores.Add("DarkMagic", new LoreObject("DarkMagic", "Dark Magic", "darkmagic_symbol", new List<string>() { TORConstants.Cultures.EMPIRE, TORConstants.Cultures.BRETONNIA, TORConstants.Cultures.ASRAI, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }, true));
                 _lores.Add("Necromancy", new LoreObject("Necromancy", "Necromancy", "necromancy_symbol", new List<string>() { TORConstants.Cultures.EMPIRE, TORConstants.Cultures.BRETONNIA, TORConstants.Cultures.ASRAI, TORConstants.Cultures.EONIR, TORConstants.Cultures.DAWI, TORConstants.Cultures.GREENSKIN }));
