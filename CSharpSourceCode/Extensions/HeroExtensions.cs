8399e74025437921baa5df39138cee96b98e0650
SlyDevil <dkmac.15@gmail.com>
2026-03-24 11:05:30 -0400

    Fix religions on wanderers now that they have no traits related to their god. Minor optimization for Imperturbable faith perk to check towns with parties rather than scanning every party. Fixed Faith's DivineMission perk granting an unassigned focus point when the player selected it with 0 unspent points. Updated a string description to clarify settlement type.

--- CSharpSourceCode/Extensions/HeroExtensions.cs
+++ CSharpSourceCode/Extensions/HeroExtensions.cs
@@ -487,6 +487,30 @@ namespace TOR_Core.Extensions
             return false;
         }
 
+        /// <summary>
+        /// Attempts to find a religion which corresponds with the first "Priest" attribute on the hero.
+        /// </summary>
+        /// <remarks>
+        /// Placeholder for wanderer religion assignment after traits were removed from their templates.
+        /// </remarks>
+        /// <returns>A ReligionObject that corresponds to the PriestXXX attribute, otherwise null.</returns>
+        public static ReligionObject GetReligionFromAttribute(this Hero hero)
+        {
+            if (hero?.GetExtendedInfo() is not HeroExtendedInfo info) return null;
+
+            var heroAttributes = info.AllAttributes;
+            foreach (var attribute in heroAttributes.WhereQ(x => !x.Equals("Priest") && x.StartsWith("Priest")))
+            {
+                var godName = attribute.ToString().Remove(0, 6);
+                godName = godName.ToLower();
+                var religion = ReligionObject.All.FirstOrDefault(x => x.StringId.Contains(godName));
+
+                if (religion != null) return religion;
+            }
+
+            return null;
+        }
+
         public static bool IsAICompanion(this Hero hero)
         {
             return hero.HasAttribute("AICompanion") && hero.Occupation == Occupation.Special;
