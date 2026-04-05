c6a1e1c3305df673d078b08e927bd4eda3831e96
Linus Tiemann <linus-tiemann@live.de>
2026-04-05 16:58:03 +0200

    remove dryad / elf penalty

--- CSharpSourceCode/Models/TORPartySizeModel.cs
+++ CSharpSourceCode/Models/TORPartySizeModel.cs
@@ -250,8 +250,6 @@ namespace TOR_Core.Models
             int treemenCount = 0;
             int minotaurCount = 0;
             int trollCount = 0;
-            int dryadCount = 0;
-            int elfCount = 0;
             int goblinCount = 0;
 
             foreach (var element in party.MemberRoster.GetTroopRoster())
@@ -269,14 +267,6 @@ namespace TOR_Core.Models
                 else if (element.Character.IsTroll())
                 {
                     trollCount += element.Number;
-                }
-                else if (element.Character.StringId == "tor_we_dryad")
-                {
-                    dryadCount += element.Number;
-                }
-                else if (element.Character.Culture?.StringId == TORConstants.Cultures.ASRAI && element.Character.IsElf())
-                {
-                    elfCount += element.Number;
                 }
                 else if (element.Character.IsGoblin())
                 {
@@ -305,26 +295,7 @@ namespace TOR_Core.Models
                 number.Add(-7 * trollCount, new TextObject("Troll weight"));
             }
 
-            // WETreekinSymbol: Dryads cost 1 slot, Elves cost 2 slots
-            // Without it: Dryads cost 2 slots, Elves cost 1 slot
-            bool hasTreekinSymbol = party.LeaderHero.HasAttribute("WETreekinSymbol");
-
-            if (hasTreekinSymbol)
-            {
-                // Elves take 2 slots each (1 extra)
-                if (elfCount > 0)
-                {
-                    number.Add(-1 * elfCount, new TextObject("Elf weight (Tree Spirit focus)"));
-                }
-            }
-            else
-            {
-                // Dryads take 2 slots each (1 extra)
-                if (dryadCount > 0)
-                {
-                    number.Add(-1 * dryadCount, new TextObject("Dryad weight"));
-                }
-            }
+            // Dryads and Elves both cost 1 slot (no weight penalty)
 
             // Goblins count as 0.5 units (half a slot each)
             if (goblinCount > 0)
