44ecae155eebae276c864568aedd3aa2c428edeb
Linus Tiemann <linus-tiemann@live.de>
2026-04-06 01:32:13 +0200

    apply performace refactor further
    dictionary is now constant, and accessible everywhere. on session load it gets instantiated, on changing perks it gets constantly refreshed
    
    this should improve somewhat performance in a lot of instances. since it reduces current O(n=30) to maybe O(n=3) by filtering massively any non applicable perks

--- CSharpSourceCode/CampaignMechanics/Careers/TORCareerPerkCampaignBehavior.cs
+++ CSharpSourceCode/CampaignMechanics/Careers/TORCareerPerkCampaignBehavior.cs
@@ -32,6 +32,7 @@ namespace TOR_Core.CampaignMechanics
     {
         public override void RegisterEvents()
         {
+            CampaignEvents.OnSessionLaunchedEvent.AddNonSerializedListener(this, OnSessionLaunched);
             CampaignEvents.DailyTickEvent.AddNonSerializedListener(this, DailyCareerTickEvents);
             CampaignEvents.WeeklyTickEvent.AddNonSerializedListener(this, WeeklyCareerTickEvents);
             CampaignEvents.ItemsLooted.AddNonSerializedListener(this, RaidingPartyEvent);
@@ -42,6 +43,12 @@ namespace TOR_Core.CampaignMechanics
             CampaignEvents.OnNewItemCraftedEvent.AddNonSerializedListener(this, OnSmithedItem);
             CampaignEvents.OnItemsRefinedEvent.AddNonSerializedListener(this, RefinedItemEvent);
             CampaignEvents.HourlyTickEvent.AddNonSerializedListener(this, HourlyPartyEvent);
+        }
+
+        private void OnSessionLaunched(CampaignGameStarter starter)
+        {
+            // Initialize career choices cache when save is loaded or new game starts
+            CareerHelper.RefreshCareerChoicesCache();
         }
 
         private void HourlyPartyEvent()
