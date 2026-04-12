07ea7ce4d1cc33496236c675dc84b70cb71d7546
alemre <aliemrebahat0033@gmail.com>
2026-04-12 07:10:29 +0200

    cross culture map event restriction fix

--- CSharpSourceCode/HarmonyPatches/MapEventJoinRestrictionsPatch.cs
+++ CSharpSourceCode/HarmonyPatches/MapEventJoinRestrictionsPatch.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
 using TaleWorlds.CampaignSystem.MapEvents;
 using TaleWorlds.CampaignSystem.Party;
 using TaleWorlds.Core;
@@ -28,6 +29,42 @@ namespace TOR_Core.HarmonyPatches
             {
                 __result = false;
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(StartBattleAction), "ApplyInternal")]
+    public static class StartBattleActionJoinRestrictionsPatch
+    {
+        [HarmonyPrefix]
+        public static bool ApplyInternalPrefix(PartyBase attackerParty, PartyBase defenderParty)
+        {
+            if (defenderParty.MapEvent == null)
+            {
+                return true;
+            }
+
+            var model = Campaign.Current.Models.GetReinforcementRestrictionModel();
+            if (model == null)
+            {
+                return true;
+            }
+
+            BattleSideEnum joinSide = defenderParty.Side == BattleSideEnum.Defender
+                ? BattleSideEnum.Attacker
+                : BattleSideEnum.Defender;
+
+            if (model.CanPartyJoinRequestedSide(defenderParty.MapEvent, attackerParty, joinSide))
+            {
+                return true;
+            }
+
+            if (attackerParty.IsMobile)
+            {
+                attackerParty.MobileParty.SetMoveModeHold();
+                attackerParty.MobileParty.Ai.RethinkAtNextHourlyTick = true;
+            }
+
+            return false;
         }
     }
 }
