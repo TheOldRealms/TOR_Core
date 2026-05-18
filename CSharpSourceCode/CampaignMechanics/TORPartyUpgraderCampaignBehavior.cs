a86f7af5d349be1f1f836198efea3fed4f2e8769
SlyDevil <dkmac.15@gmail.com>
2026-05-18 17:52:42 -0400

    Protecting PartyBase.Culture calls against null MapFactions. Fixed dwarf grudges not applying to relevant bandits.

--- CSharpSourceCode/CampaignMechanics/TORPartyUpgraderCampaignBehavior.cs
+++ CSharpSourceCode/CampaignMechanics/TORPartyUpgraderCampaignBehavior.cs
@@ -137,12 +137,12 @@ namespace TOR_Core.CampaignMechanics
             
             //exclude heroes from the count so the composition is only based on troops
             float troopCount = memberRoster.TotalRegulars;
-            var defaultTemplate = party.MobileParty.ActualClan?.DefaultPartyTemplate ?? party.Culture?.DefaultPartyTemplate;//bandit and mercenary clan parties use a template taken from their Clan object, and if a clan has no party template then it falls back to the cultural template. The culture fallback exists here because RaidingPartyComponents don't have a clan atm, they are only assigned to a hero.
+            var defaultTemplate = party.MobileParty.ActualClan?.DefaultPartyTemplate ?? party.MapFaction?.Culture?.DefaultPartyTemplate;//bandit and mercenary clan parties use a template taken from their Clan object, and if a clan has no party template then it falls back to the cultural template. The culture fallback exists here because RaidingPartyComponents don't have a clan atm, they are only assigned to a hero.
 
             var cultureTemplateStacks = defaultTemplate?.Stacks;
             Dictionary<CharacterObject, float> missingTroopsAtEachTier = []; //these could probably be stored as ints and rounded up at each step as the precision doesn't really matter; it will give slightly stronger parties.
             
-            var partyCulture = party.MobileParty?.ActualClan?.Culture ?? party.Culture;
+            var partyCulture = party.MobileParty?.ActualClan?.Culture ?? party.MapFaction?.Culture;
             if (partyCulture != null && _cultureTemplateData.TryGetValue(partyCulture.StringId, out var cultureData) && cultureData != null)
             {
                 float totalTroopsInTemplate = cultureData.Item1;
@@ -232,7 +232,7 @@ namespace TOR_Core.CampaignMechanics
 
         private bool BanditPerkAndItemCheck(PartyBase party, CharacterObject troopCharacter, CharacterObject upgradeTargetCharacter, PartyTroopUpgradeModel partyTroopUpgradeModel)
         {
-            return (!party.Culture.IsBandit || upgradeTargetCharacter.Culture.IsBandit) && (troopCharacter.Occupation != Occupation.Bandit || partyTroopUpgradeModel.CanPartyUpgradeTroopToTarget(party, troopCharacter, upgradeTargetCharacter));
+            return (((party.MapFaction?.Culture)?.IsBandit == true) || upgradeTargetCharacter.Culture.IsBandit) && (troopCharacter.Occupation != Occupation.Bandit || partyTroopUpgradeModel.CanPartyUpgradeTroopToTarget(party, troopCharacter, upgradeTargetCharacter));
         }
 
         private List<TORTroopUpgradeArgs> GetPossibleUpgradeTargets(PartyBase party, TroopRosterElement rosterElement)
