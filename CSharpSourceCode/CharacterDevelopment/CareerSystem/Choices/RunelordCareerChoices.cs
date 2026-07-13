9a604c2adf2f0b75bac67321c86c0adda083b85e
SlyDevil <dkmac.15@gmail.com>
2026-07-13 17:21:44 -0400

    Fixes Wisdom of Thungni not scaling at the stated value with smithing levels.

--- CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/RunelordCareerChoices.cs
+++ CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/RunelordCareerChoices.cs
@@ -119,7 +119,7 @@ public class RunelordCareerChoices(CareerObject id) : TORCareerChoicesBase(id)
                     MutationTargetType = typeof(AbilityTemplate),
                     MutationTargetOriginalId = "WisdomOfThungni",
                     PropertyName = "ScaleVariable1",
-                    PropertyValue = (choice, originalValue, agent) =>  CareerHelper.AddSkillEffectToValue(choice, agent, new List<SkillObject>(){ DefaultSkills.Crafting }, 0.009f),
+                    PropertyValue = (choice, originalValue, agent) =>  CareerHelper.AddSkillEffectToValue(choice, agent, new List<SkillObject>(){ DefaultSkills.Crafting }, 0.1f),
                     MutationType = OperationType.Add
                 },
             });
