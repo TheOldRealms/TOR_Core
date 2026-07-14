710a32fc7f8c68e3850d7917340ed308fc8575fe
SlyDevil <dkmac.15@gmail.com>
2026-07-14 10:33:09 -0400

    Removes Wisdom of Thungni's dependency on a specific item trait existing.

--- CSharpSourceCode/AbilitySystem/Scripts/WisdomOfThungniScript.cs
+++ CSharpSourceCode/AbilitySystem/Scripts/WisdomOfThungniScript.cs
@@ -28,10 +28,12 @@ public class WisdomOfThungniScript : CareerAbilityScript
 
 
         var trait = ItemTrait.All.FirstOrDefaultQ(x => x.ItemTraitStringId == "magical_weapon_10");
-        if (trait == null) return;
+        if (trait != null)
+        {
+            var comp = agent.GetComponent<ItemTraitAgentComponent>();
+            if (comp != null) comp.AddTraitToWieldedWeapon(trait, ability.Template.Duration);
+        }
 
-        var comp = agent.GetComponent<ItemTraitAgentComponent>();
-        if (comp != null) comp.AddTraitToWieldedWeapon(trait, ability.Template.Duration);
 
         foreach (var element in abilityComponent.KnownAbilitySystem)
         {
