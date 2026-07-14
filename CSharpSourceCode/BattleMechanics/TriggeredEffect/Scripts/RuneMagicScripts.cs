22d975c05ef1e985cde3e871f09a6480028b05e0
SlyDevil <dkmac.15@gmail.com>
2026-07-14 11:50:19 -0400

    Comment for when rewriting existing code.

--- CSharpSourceCode/BattleMechanics/TriggeredEffect/Scripts/RuneMagicScripts.cs
+++ CSharpSourceCode/BattleMechanics/TriggeredEffect/Scripts/RuneMagicScripts.cs
@@ -72,7 +72,7 @@ public class SpellbreakerRuneScript : ITriggeredScript
             if (agent.Team.IsPlayerAlly)
             {
                 var component = agent.GetComponent<StatusEffectComponent>();
-                component.RemoveStatusEffect("", StatusEffectComponent.EffectFlag.Enemy);
+                component.RemoveStatusEffect("", StatusEffectComponent.EffectFlag.Enemy);//Sly : why is an empty status effect attempting to be removed? This will do nothing because status effects with empty ids will be filtered out before attempting addition to a component.
             }
 
             if (!agent.IsSpellCaster()) continue;
