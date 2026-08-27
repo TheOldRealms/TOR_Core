d2426968df248034bba3caf961c96a1e53b0d406
SlyDevil <dkmac.15@gmail.com>
2026-08-27 16:48:09 -0400

    Fixes a tournament participant filter only excluding treemen rather than all Monstrous races including newer ones.

--- CSharpSourceCode/BattleMechanics/CustomArenaModes/ArcheryContestTournamentGame.cs
+++ CSharpSourceCode/BattleMechanics/CustomArenaModes/ArcheryContestTournamentGame.cs
@@ -20,7 +20,7 @@ namespace TOR_Core.BattleMechanics.CustomArenaModes
 
         public override bool CanBeAParticipant(CharacterObject character, bool considerSkills)
         {
-            if (character.Race == FaceGen.GetRaceOrDefault("large_humanoid_monster") ||
+            if (character.IsMonstrous() ||
                 character.HasAttribute("HasAnimationTriggeredEffects") ||
                 character.Culture?.StringId == "chaos_culture")
             {
