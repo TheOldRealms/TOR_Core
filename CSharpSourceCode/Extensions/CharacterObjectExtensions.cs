TheOldRealms/TOR_Core

commit 799baa443e0f96bb41d603f1aa0dd585783042d2

Centralization continues. All attributes except for handful related to specific career perks and troop-applied-buff mechanics (seals, powerstones, etc...) are missing.

CSharpSourceCode/Extensions/CharacterObjectExtensions.cs
@@ -60,7 +60,7 @@ namespace TOR_Core.Extensions
 
         public static bool IsMonstrous(this BasicCharacterObject characterObject)
         {
-            return characterObject != null && characterObject.HasAttribute("Monstrous");
+            return characterObject != null && characterObject.HasAttribute(CharacterAttributes.MONSTROUS);
         }
 
         public static bool CanClimbLadders(this BasicCharacterObject characterObject)
@@ -113,21 +113,17 @@ namespace TOR_Core.Extensions
             {
                 return characterObject.HeroObject.IsUndead();
             }
-            return characterObject.GetAttributes().Contains("Undead");
+            return characterObject.HasAttribute(CharacterAttributes.UNDEAD);
         }
-
-        public static bool IsGhost(this CharacterObject characterObject)
-        {
-            return characterObject.StringId.Contains("spirit_host") || characterObject.StringId.Contains("wraith");
-        }
-
+                
         public static bool IsVampire(this CharacterObject characterObject)
         {
-            if (characterObject.IsHero)
-            {
-                return characterObject.HeroObject.IsVampire();
-            }
             return characterObject.Race == FaceGen.GetRaceOrDefault("vampire") || characterObject.Race == FaceGen.GetRaceOrDefault("necrarch");
+        }
+
+        public static bool IsNecrarch(this BasicCharacterObject basicCharacterObject)
+        {
+            return basicCharacterObject.Race == FaceGen.GetRaceOrDefault("necrarch");
         }
         
         public static bool IsDwarf(this BasicCharacterObject characterObject)
@@ -137,27 +133,15 @@ namespace TOR_Core.Extensions
 
         public static bool IsDwarf(this CharacterObject characterObject)
         {
-            if (characterObject.IsHero)
-            {
-                return characterObject.HeroObject.IsDwarf();
-            }
             return characterObject.Race == FaceGen.GetRaceOrDefault("dwarf");
         }
 
         public static bool IsOrc(this CharacterObject characterObject)
         {
-            if (characterObject.IsHero)
-            {
-                return characterObject.HeroObject.IsOrc();
-            }
             return characterObject.Race == FaceGen.GetRaceOrDefault("orc");
         }
         public static bool IsGoblin(this CharacterObject characterObject)
         {
-            if (characterObject.IsHero)
-            {
-                return characterObject.HeroObject.IsGoblin();
-            }
             return characterObject.Race == FaceGen.GetRaceOrDefault("goblin");
         }
 
@@ -176,9 +160,9 @@ namespace TOR_Core.Extensions
             return characterObject.Race == FaceGen.GetRaceOrDefault("troll");
         }
 
-        public static bool IsMinotaur(this BasicCharacterObject characterObject)
+        public static bool IsMinotaur(this BasicCharacterObject basicCharacterObject)
         {
-            return characterObject.Race == FaceGen.GetRaceOrDefault("minotaur");
+            return basicCharacterObject.Race == FaceGen.GetRaceOrDefault("minotaur");
         }
 
         public static bool IsTreeman(this BasicCharacterObject basicCharacterObject)
@@ -214,17 +198,17 @@ namespace TOR_Core.Extensions
         public static bool IsTreeSpirit(this CharacterObject characterObject)
         {
             if (characterObject.IsHero) return characterObject.HeroObject.IsTreeSpirit();
-            else return characterObject.GetAttributes().Contains("TreeSpirit");
+            else return characterObject.HasAttribute(CharacterAttributes.TREE_SPIRIT);
         }
 
         public static bool IsTreeSpirit(this BasicCharacterObject characterObject)
         {
-            return characterObject.GetAttributes().Contains("TreeSpirit");
+            return characterObject.HasAttribute(CharacterAttributes.TREE_SPIRIT);
         }
 
         public static bool IsKnightUnit(this CharacterObject characterObject)
         {
-            return !characterObject.IsHero && characterObject.IsMounted && (IsEliteTroop(characterObject) || characterObject.HasAttribute("Knightly"));
+            return !characterObject.IsHero && characterObject.IsMounted && (IsEliteTroop(characterObject) || characterObject.HasAttribute(CharacterAttributes.KNIGHTLY));
         }
 
         public static bool IsRunesmith(this CharacterObject characterObject)
@@ -313,7 +297,7 @@ namespace TOR_Core.Extensions
 
         public static bool IsUndead(this BasicCharacterObject characterObject)
         {
-            return characterObject.GetAttributes().Contains("Undead");
+            return characterObject.HasAttribute(CharacterAttributes.UNDEAD);
         }
 
         public static bool IsSkeleton(this BasicCharacterObject characterObject)
@@ -321,14 +305,19 @@ namespace TOR_Core.Extensions
             return characterObject.Race == FaceGen.GetRaceOrDefault("skeleton");
         }
 
-        public static bool IsGhost(this BasicCharacterObject characterObject)
+        public static bool IsGhost(this CharacterObject characterObject)
         {
-            return characterObject.StringId.Contains("spirit_host") || characterObject.StringId.Contains("wraith");
+            return characterObject.Race == FaceGen.GetRaceOrDefault("spirit_host") || characterObject.Race == FaceGen.GetRaceOrDefault("wraith");
+        }
+
+        public static bool IsGhost(this BasicCharacterObject basicCharacterObject)
+        {
+            return basicCharacterObject.Race == FaceGen.GetRaceOrDefault("spirit_host") || basicCharacterObject.Race == FaceGen.GetRaceOrDefault("wraith");
         }
 
         public static bool IsIronbreakerUnit(this BasicCharacterObject characterObject)
         {
-            return characterObject.IsSoldier && characterObject.Culture.StringId == TORConstants.Cultures.DAWI && characterObject.StringId.Contains("iron");
+            return characterObject.IsSoldier && characterObject.Culture.StringId == TORConstants.Cultures.DAWI && characterObject.HasAttribute(CharacterAttributes.IRONBREAKER);
         }
 
         public static bool IsVampire(this BasicCharacterObject characterObject)
@@ -353,12 +342,12 @@ namespace TOR_Core.Extensions
 
         public static bool IsBloodDragon(this BasicCharacterObject characterObject)
         {
-            return characterObject.GetAttributes().Contains("BloodDragon");
+            return characterObject.IsVampire() && characterObject.HasAttribute(CharacterAttributes.BLOOD_DRAGON);
         }
 
         public static bool IsBrassKeepLord(this BasicCharacterObject characterObject)
         {
-            return characterObject.GetAttributes().Contains("BrassKeep");
+            return characterObject.HasAttribute(CharacterAttributes.BRASS_KEEP);
         }
 
         public static bool IsReligiousUnit(this CharacterObject characterObject)
@@ -433,19 +422,20 @@ namespace TOR_Core.Extensions
                     var explainedNumber = new ExplainedNumber(cost);
                     CareerHelper.ApplyBasicCareerPassives(Hero.MainHero, ref explainedNumber, PassiveEffectType.CustomResourceUpgradeCostModifier, true, character);
 
-                    // Waaagh3 and Waaagh4 (Wargh3/Wargh4): Teef upgrade penalty for Greenskins
+                    
+                    
+                    
+                    
+                    // Waaagh3 and Waaagh4 : Teef upgrade penalty for Greenskins
                     if (Hero.MainHero.Culture.StringId == TORConstants.Cultures.GREENSKIN &&
-                        info.ResourceCost.ResourceType == "Teef")
+                        info.ResourceCost.ResourceType == "Teef")//resource type check is redundant because upgrades should be disabled prior to this for other culture troops who would cost a different resource.
+                        //Perhaps this would change if goblins had a different currency, but atm not relevant.
                     {
-                        if (Hero.MainHero.HasAttribute("Wargh4"))
+                        if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_3))//Sly : these are spelled wrong and need to point at the Waaagh attributes
                             explainedNumber.AddFactor(1.0f);
-                        else if (Hero.MainHero.HasAttribute("Wargh3"))
-                            explainedNumber.AddFactor(0.5f);
+                        else if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_2))cost = Math.Max((int)explainedNumber.ResultNumber, 1);
+                }           explainedNumber.AddFactor(0.5f);
                     }
-
-                    cost = Math.Max((int)explainedNumber.ResultNumber, 1);
-                }
-
                 return new Tuple<CustomResource, int>(CustomResourceManager.GetResourceObject(info.ResourceCost.ResourceType), cost);
             }
             return null;

CSharpSourceCode/AbilitySystem/Ability.cs
@@ -256,7 +256,7 @@ namespace TOR_Core.AbilitySystem
                                 {
                                     if (agent.GetHero().CharacterObject.IsRunesmith())
                                     {
-                                        cooldown.AddFactor(-0.02f);
+                                        cooldown.AddFactor(-0.02f);//Sly : this isn't what the perk says it does.
                                     }
                                 }
                             }

CSharpSourceCode/AbilitySystem/Scripts/AccusationScript.cs
@@ -5,6 +5,7 @@ using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 using TOR_Core.BattleMechanics.StatusEffect;
 using TOR_Core.Extensions;
+using static TOR_Core.Utilities.TORConstants;
 
 namespace TOR_Core.AbilitySystem.Scripts
 {
@@ -75,7 +76,7 @@ namespace TOR_Core.AbilitySystem.Scripts
 
                 var tempAttributes = target.GetComponent<StatusEffectComponent>().GetTemporaryAttributes();
 
-                if (tempAttributes.Contains("AccusationMark"))
+                if (tempAttributes.Contains(CharacterAttributes.ACCUSATION_MARK))
                 {
                     validTargets.Remove(target);
                 }

CSharpSourceCode/BattleMechanics/CareerPerkMissionBehavior.cs
@@ -155,7 +155,7 @@ namespace TOR_Core.BattleMechanics
             var comp = affectedAgent.GetComponent<StatusEffectComponent>();
             if (comp == null) return;
             var temporaryEffects = comp.GetTemporaryAttributes();
-            if (!temporaryEffects.Contains("AccusationMark")) return;
+            if (!temporaryEffects.Contains(CharacterAttributes.ACCUSATION_MARK)) return;
 
             var choices = Hero.MainHero.GetAllCareerChoices();
 

CSharpSourceCode/CampaignMechanics/CharacterCreation/TORCharacterCreationContentHandler.cs
@@ -805,13 +805,13 @@ namespace TOR_Core.CampaignMechanics.CharacterCreation
                     break;
 
                 case "option_3_vc_vampire":
-                    hero.AddAttribute("Vampire");
+                    hero.AddAttribute(CharacterAttributes.VAMPIRE);
                     hero.AddAttribute(CharacterAttributes.NECROMANCER);
                     hero.AddReligiousInfluence(ReligionObject.All.FirstOrDefault(x => x.StringId == "cult_of_nagash"), 60);
                     break;
 
                 case "option_3_mousillon_vampire":
-                    hero.AddAttribute("Vampire");
+                    hero.AddAttribute(CharacterAttributes.VAMPIRE);
                     hero.AddAttribute(CharacterAttributes.NECROMANCER);
                     hero.AddReligiousInfluence(ReligionObject.All.FirstOrDefault(x => x.StringId == "cult_of_nagash"), 60);
                     // Bloodline career will be applied by ApplyStoredSpecializations
@@ -979,7 +979,7 @@ namespace TOR_Core.CampaignMechanics.CharacterCreation
                     {
                         hero.AddCareer(TORCareers.WarriorPriest);
                         hero.AddReligiousInfluence(ReligionObject.All.FirstOrDefault(x => x.StringId == "cult_of_sigmar"), 60);
-                        hero.AddAttribute("PriestSigmar");
+                        hero.AddAttribute(CharacterAttributes.PRIEST_SIGMAR);
                         var skill = hero.GetSkillValue(TORSkills.Faith);
                         hero.HeroDeveloper.SetInitialSkillLevel(TORSkills.Faith, Math.Max(skill, 25));
                         hero.HeroDeveloper.AddPerk(TORPerks.Faith.NovicePrayers);
@@ -997,7 +997,7 @@ namespace TOR_Core.CampaignMechanics.CharacterCreation
                     {
                         hero.AddCareer(TORCareers.WarriorPriestUlric);
                         hero.AddReligiousInfluence(ReligionObject.All.FirstOrDefault(x => x.StringId == "cult_of_ulric"), 60);
-                        hero.AddAttribute("PriestUlric");
+                        hero.AddAttribute(CharacterAttributes.PRIEST_ULRIC);
                         var skill = hero.GetSkillValue(TORSkills.Faith);
                         hero.HeroDeveloper.SetInitialSkillLevel(TORSkills.Faith, Math.Max(skill, 25));
                         hero.HeroDeveloper.AddPerk(TORPerks.Faith.NovicePrayers);

CSharpSourceCode/CampaignMechanics/SpellTrainers/SpellTrainerInTownBehavior.cs
@@ -745,7 +745,7 @@ namespace TOR_Core.CampaignMechanics.SpellTrainers
             if (Hero.MainHero.HasCareer(TORCareers.GrailDamsel) && !damselCompanion.HasKnownLore("LoreOfBeasts"))
             {
                 GameTexts.SetVariable("DAMSELNAME", damselCompanion.Name);
-                return Hero.MainHero.HasAttribute("SecondLoreForDamselCompanions");
+                return Hero.MainHero.HasAttribute(CharacterAttributes.CAREER_TIER_3);
             }
             return false;
         }

CSharpSourceCode/CampaignMechanics/TORAIRecruitmentCampaignBehavior.cs
@@ -12,6 +12,7 @@ using TaleWorlds.LinQuick;
 using TaleWorlds.ObjectSystem;
 using TOR_Core.Extensions;
 using TOR_Core.Utilities;
+using static TOR_Core.Utilities.TORConstants;
 
 namespace TOR_Core.CampaignMechanics
 {
@@ -81,7 +82,7 @@ namespace TOR_Core.CampaignMechanics
             if (artilleryCrew == null) return;
 
             var crewInPartySum = party.MemberRoster.GetTroopRoster()
-                .Where(x => x.Character.GetAttributes().Contains("ArtilleryCrew"))
+                .Where(x => x.Character.HasAttribute(CharacterAttributes.ARTILLERY_CREW))
                 .Sum(x => x.Number);
 
             if (crewInPartySum > 10) { return; } //2 crew per gun, max 3 guns for certain ai parties
@@ -111,8 +112,8 @@ namespace TOR_Core.CampaignMechanics
                         }
 
                         if (party.Party.PartySizeLimit > party.MemberRoster.TotalManCount + UndeadCountTowns)
-                        {
-                            var count = party.LeaderHero.HasAttribute("BloodDragon") ? UndeadCountVillages : UndeadCountTowns;
+                        {//Sly : this can use the IsBloodDragon extension instead
+                            var count = party.LeaderHero.HasAttribute(CharacterAttributes.BLOOD_DRAGON) ? UndeadCountVillages : UndeadCountTowns;
                             party.MemberRoster.AddToCounts(_skeleton, count, false, 0);
 
                             if (party.ActualClan.StringId.Contains("necrarch"))
@@ -135,11 +136,11 @@ namespace TOR_Core.CampaignMechanics
                 }
 
                 // SlayerLord Recruitment in Dwarf Towns
-                if (party.LeaderHero.HasAttribute("SlayerLord") &&
+                if (party.LeaderHero.HasAttribute(CharacterAttributes.SLAYER_LORD) &&
                     party.CurrentSettlement != null &&
                     party.CurrentSettlement.IsDwarfKarak())
                 {
-                    ProcessSlayerRecruitment(party);
+                    ProcessSlayerRecruitment(party);//Sly : this is confusing as to why it's separate from the process of adding slayers to the party when entering a settlement
                 }
             }
         }
@@ -221,7 +222,7 @@ namespace TOR_Core.CampaignMechanics
         {
             if (party == null || settlement == null || leaderHero == null) return;
             if (party.IsMainParty || settlement.IsHideout) return;
-            if (!leaderHero.HasAttribute("SlayerLord")) return;
+            if (!leaderHero.HasAttribute(CharacterAttributes.SLAYER_LORD)) return;
             if (!settlement.IsDwarfKarak()) return;
             if (_slayer == null) return;
 
@@ -377,7 +378,7 @@ namespace TOR_Core.CampaignMechanics
             if (verifiedAmount == null || verifiedAmount < 1) return;
             amount = (int)verifiedAmount; //Sly : if multiple behaviours remove troops (which makes use of the roster index), then the roster index may vary and a troop with no valid index causes the troop roster to fetch data[-1], ie. AssimilationCampaignBehavior
 
-            if (recruiter.CharacterObject.IsBloodDragon())
+            if (recruiter.IsBloodDragon())
             {
                 if (troop.StringId == "tor_vc_vampire_newblood") return;
                 int count = 0;
@@ -396,7 +397,7 @@ namespace TOR_Core.CampaignMechanics
                 }
             }
 
-            if (recruiter.HasAttribute("Everchosen"))
+            if (recruiter.HasAttribute(CharacterAttributes.EVERCHOSEN))
             {
                 CharacterObject replacement = null;
 
@@ -416,12 +417,12 @@ namespace TOR_Core.CampaignMechanics
             if (troop.IsEliteTroop() && recruiter.Culture.StringId == TORConstants.Cultures.BRETONNIA)
             {
                 CharacterObject replacement = null;
-                if (recruiter.HasAttribute("Bergerac"))
+                if (recruiter.HasAttribute(CharacterAttributes.BERGERAC))
                 {
                     replacement = MBObjectManager.Instance.GetObject<CharacterObject>("tor_ror_bergerac_ranger");
                 }
 
-                if (recruiter.HasAttribute("PeasantKnight"))
+                if (recruiter.HasAttribute(CharacterAttributes.PEASANT_KNIGHT))
                 {
                     replacement = MBObjectManager.Instance.GetObject<CharacterObject>("tor_ror_peasant_squight");
                 }
@@ -458,7 +459,7 @@ namespace TOR_Core.CampaignMechanics
 
             if (recruiter.IsLord && troop.Culture.StringId == TORConstants.Cultures.MOUSILLON && recruiter.Culture.StringId == TORConstants.Cultures.BRETONNIA)
             {
-                var mousillonEquivalent = TORRecruitmentHelpers.GetMousillonEquivalent(troop);
+                var mousillonEquivalent = TORRecruitmentHelpers.GetMousillonEquivalent(troop);//Sly : this is inverted, it's taking a mousillon troop, and finding a different mousillon troop to give to a bretonnian.
                 if (mousillonEquivalent != null)
                 {
                     recruiter.PartyBelongedTo.Party.AddMember(mousillonEquivalent, amount);
@@ -468,7 +469,7 @@ namespace TOR_Core.CampaignMechanics
 
             if (recruiter.IsLord && troop.Culture.StringId == TORConstants.Cultures.BRETONNIA && recruiter.Culture.StringId == TORConstants.Cultures.MOUSILLON)
             {
-                var bretonniaEquivalent = TORRecruitmentHelpers.GetBretonnianEquivalent(troop);
+                var bretonniaEquivalent = TORRecruitmentHelpers.GetBretonnianEquivalent(troop);//Sly : same culture inversion as above.
                 if (bretonniaEquivalent != null)
                 {
                     recruiter.PartyBelongedTo.Party.AddMember(bretonniaEquivalent, amount);

CSharpSourceCode/CharacterDevelopment/CareerSystem/CareerButton/KnightOldWorldCareerButtonBehavior.cs
@@ -214,7 +214,7 @@ public class KnightOldWorldCareerButtonBehavior : CareerButtonBehaviorBase
 
         if (characterObject.IsKnightUnit() && characterObject.Culture.StringId != TORConstants.Cultures.BRETONNIA) return true;
 
-        if (characterObject.HasAttribute("Knightly"))
+        if (characterObject.HasAttribute(CharacterAttributes.KNIGHTLY))
         {
             return true;
         }

commit b653ab9a8149a2ce9dd6c7001bb1f1af2e637c1c

Fixes a bug introduced in commit 799baa443e0f96bb41d603f1aa0dd585783042d2 that prevented CustomResource UpgradeCosts factors from being applied.

CSharpSourceCode/Extensions/CharacterObjectExtensions.cs
@@ -417,14 +417,11 @@ namespace TOR_Core.Extensions
             if (info != null && character.HasCustomResourceUpgradeRequirement())
             {
                 var cost = info.ResourceCost.UpgradeCost;
+
                 if (belongsToMainParty)
                 {
                     var explainedNumber = new ExplainedNumber(cost);
                     CareerHelper.ApplyBasicCareerPassives(Hero.MainHero, ref explainedNumber, PassiveEffectType.CustomResourceUpgradeCostModifier, true, character);
-
-                    
-                    
-                    
                     
                     // Waaagh3 and Waaagh4 : Teef upgrade penalty for Greenskins
                     if (Hero.MainHero.Culture.StringId == TORConstants.Cultures.GREENSKIN &&
@@ -433,9 +430,14 @@ namespace TOR_Core.Extensions
                     {
                         if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_3))
                             explainedNumber.AddFactor(1.0f);
-                        else if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_2))cost = Math.Max((int)explainedNumber.ResultNumber, 1);
-                }           explainedNumber.AddFactor(0.5f);
+                        else if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_2))
+                            explainedNumber.AddFactor(0.5f);
                     }
+
+                    
+                    cost = Math.Max((int)explainedNumber.ResultNumber, 1);
+                }
+
                 return new Tuple<CustomResource, int>(CustomResourceManager.GetResourceObject(info.ResourceCost.ResourceType), cost);
             }
             return null;