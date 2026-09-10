69d381df8f3a8c653639067ff6c99290b83674a7
SlyDevil <dkmac.15@gmail.com>
2026-09-10 15:35:06 -0400

    Remove outdated comment for a CharacterAttribute.

--- CSharpSourceCode/Extensions/CharacterObjectExtensions.cs
+++ CSharpSourceCode/Extensions/CharacterObjectExtensions.cs
@@ -431,7 +431,7 @@ namespace TOR_Core.Extensions
                         info.ResourceCost.ResourceType == "Teef")//resource type check is redundant because upgrades should be disabled prior to this for other culture troops who would cost a different resource.
                         //Perhaps this would change if goblins had a different currency, but atm not relevant.
                     {
-                        if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_3))//Sly : these are spelled wrong and need to point at the Waaagh attributes
+                        if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_3))
                             explainedNumber.AddFactor(1.0f);
                         else if (Hero.MainHero.HasAttribute(CharacterAttributes.WAAAAGH_2))cost = Math.Max((int)explainedNumber.ResultNumber, 1);
                 }           explainedNumber.AddFactor(0.5f);
