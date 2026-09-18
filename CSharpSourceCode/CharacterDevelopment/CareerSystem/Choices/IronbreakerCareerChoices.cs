66353721de806f4b22211423584383fe9e83f413
Dannyboi <277157194+Dannyboi802@users.noreply.github.com>
2026-09-18 16:40:11 -0600

    Remove obsolete Irondrake Oathgold workaround and update description

--- CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/IronbreakerCareerChoices.cs
+++ CSharpSourceCode/CharacterDevelopment/CareerSystem/Choices/IronbreakerCareerChoices.cs
@@ -329,7 +329,7 @@ public class IronbreakerCareerChoices(CareerObject id) : TORCareerChoicesBase(id
 
         _ironDrakesPassive1.Initialize(CareerID, "+20% Fire damage for 'Irondrake' troops.", "IronDrakes", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(PassiveEffectType.TroopDamage, new DamageProportionTuple(DamageType.Fire, 20), AttackTypeMask.Ranged,
             (attacker, victim, mask) => attacker.IsPlayerUnit && !attacker.IsHero && attacker.Character.IsIronbreakerUnit() && attacker.Character.HasAttribute(CharacterAttributes.DWARF_GUN)));
-        _ironDrakesPassive2.Initialize(CareerID, "-25% 'Oathgold' upgrade cost for Ironbreakers, Ironbeards, Irondrakes, and Trollhammer Irondrakes.", "IronDrakes", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(-25, PassiveEffectType.CustomResourceUpgradeCostModifier, true,
+        _ironDrakesPassive2.Initialize(CareerID, "-25% Oathgold upgrade cost for the Ironbreaker troop line.", "IronDrakes", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect(-25, PassiveEffectType.CustomResourceUpgradeCostModifier, true,
             characterObject => characterObject.HasAttribute(CharacterAttributes.IRONBREAKER)));
         _ironDrakesPassive3.Initialize(CareerID, "+12 ammunition for Drakefire canisters carried by you and companions in your party.", "IronDrakes", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect());
         _ironDrakesPassive4.Initialize(CareerID, "+1% ammunition for 'Ironbreaker' troops per Ironbeard unit.", "IronDrakes", false, ChoiceType.Passive, null, new CareerChoiceObject.PassiveEffect());
